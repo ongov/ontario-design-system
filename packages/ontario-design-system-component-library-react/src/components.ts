bce655d9891c67dac5d41dd2a7fb43cacfaee1b7
Scott Morris <scott.morris@ontario.ca>
2021-10-18 14:50:18 -0400

    Add component-library-react package
    - Wire it up with the ontario-design-system-component-library package

---
+++ packages/ontario-design-system-component-library-react/src/components.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'ontario-design-system-component-library';
+
+import { defineCustomElements } from 'ontario-design-system-component-library/loader';
+
+defineCustomElements();
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
