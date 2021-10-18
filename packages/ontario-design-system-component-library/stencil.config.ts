e981fa37df68ef19c83426265cd4699155f54496
Scott Morris <scott.morris@ontario.ca>
2021-10-18 12:07:28 -0400

    Reorganize base project
    - Prep it for being a monorepo.

---
+++ packages/ontario-design-system-component-library/stencil.config.ts
@@ -0,0 +1,30 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+import { reactOutputTarget } from '@stencil/react-output-target';
+
+export const config: Config = {
+	namespace: 'ontario-design-system-components',
+	plugins: [sass()],
+	buildEs5: 'prod',
+	outputTargets: [
+		reactOutputTarget({
+			componentCorePackage: 'ontario-design-system-component-library',
+			proxiesFile: '../ontario-design-system-component-library-react/src/components.ts',
+			includeDefineCustomElements: true,
+		}),
+		{
+			type: 'dist',
+			esmLoaderPath: '../loader',
+		},
+		{
+			type: 'dist-custom-elements-bundle',
+		},
+		{
+			type: 'docs-readme',
+		},
+		{
+			type: 'www',
+			serviceWorker: null, // disable service workers
+		},
+	],
+};
