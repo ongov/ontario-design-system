66461d24a78541a1d05c1f98596dba1b12a8531f
Erin Carrieres <erin.carrieres@ontario.ca>
2021-11-10 11:01:48 -0500

    Initial Design Tokens

---
+++ packages/ontario-design-tokens/build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Tue, 09 Nov 2021 17:47:36 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
