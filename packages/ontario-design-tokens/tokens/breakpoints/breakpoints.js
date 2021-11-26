b85abdb9bc2cdf212e223a1f7f4980be2feebe93
Erin Carrieres <erin.carrieres@ontario.ca>
2021-11-26 15:11:58 -0500

    Updated design tokens to js

---
+++ packages/ontario-design-tokens/tokens/breakpoints/breakpoints.js
@@ -0,0 +1,10 @@
+// config.js
+module.exports = {
+  ontarioBreakpoints: {
+    xsmall: { value: "20em"},
+    small: { value: "40em"},
+    medium: { value: "73em"},
+    large: { value: "96em"},
+    xlarge: { value: "120em"}
+  }
+}
---
+++ packages/ontario-design-tokens/tokens/global/global.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ontarioGlobalTokens: {
+    radius: { value: "4px" },
+    pixelValue: { value: "0.063em" },
+    maxValue: { value: "999999999999" }
+  }
+}
