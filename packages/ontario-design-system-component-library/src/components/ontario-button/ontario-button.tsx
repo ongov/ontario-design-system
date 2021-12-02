6cf7231207bab8acec78f030af083bc3b2166e41
Elsa Ng <elsa.ng@ontario.ca>
2021-12-01 21:30:05 -0500

    Add comments to properties
    Comments are added to properties used in the component
    so that the component's readme.md file can be
    automatically generated by StencilJS

--- packages/ontario-design-system-component-library/src/components/ontario-button/ontario-button.tsx
+++ packages/ontario-design-system-component-library/src/components/ontario-button/ontario-button.tsx
@@ -10,9 +10,21 @@ import { Component, Prop, Element, h } from '@stencil/core';
 })
 export class OntarioButton {
 	@Element() host: HTMLElement;
+	/**
+	 * Sets the type of button that the Ontario Design System uses.
+	 */
 	@Prop() type: 'primary' | 'secondary' | 'tertiary' = 'secondary';
+	/**
+	 * Sets the native HTML button type attribute.
+	 */
 	@Prop() htmlType: 'button' | 'reset' | 'submit' = 'button';
+	/**
+	 * Sets text to display within the button. This will override the text provided through the Element Content.
+	 */
 	@Prop() label: string;
+	/**
+	 * Overrides the default value of the aria-label attribute.
+	 */
 	@Prop() ariaLabel: string;
 
 	private getButtonLabel() {
