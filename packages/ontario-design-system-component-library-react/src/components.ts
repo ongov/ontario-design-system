6f096a360e4fb1868f52783edd835c247ffb6a99
Meaghan Carrieres <meaghan.carriers@ontario.ca>
2021-12-21 15:53:42 -0500

    * Generates text input component structure with stencil generator (.scss, .tsx and tests folder) * Updates README according to doc specifications * Adds UUID package + updates package.lock version file

--- packages/ontario-design-system-component-library-react/src/components.ts
+++ packages/ontario-design-system-component-library-react/src/components.ts
@@ -10,3 +10,4 @@ import { defineCustomElements } from 'ontario-design-system-component-library/lo
 defineCustomElements();
 export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
 export const OntarioButton = /*@__PURE__*/createReactComponent<JSX.OntarioButton, HTMLOntarioButtonElement>('ontario-button');
+export const OntarioInput = /*@__PURE__*/createReactComponent<JSX.OntarioInput, HTMLOntarioInputElement>('ontario-input');
