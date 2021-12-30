8776975e6482808a0197a269a680800212afc486
Erin Carrieres <erin.carrieres@ontario.ca>
2021-12-30 12:03:38 -0500

    Added snapshot test

--- packages/ontario-design-system-component-library/src/components/ontario-hint-text/test/ontario-hint-text.spec.tsx
+++ packages/ontario-design-system-component-library/src/components/ontario-hint-text/test/ontario-hint-text.spec.tsx
@@ -2,6 +2,17 @@ import { newSpecPage } from '@stencil/core/testing';
 import { OntarioHintText } from '../ontario-hint-text';
 
 describe('ontario-hint-text', () => {
+  describe('snapshot', () => {
+    it('should render the expected html', async () => {
+      const page = await newSpecPage({
+        components: [OntarioHintText],
+        html: `<ontario-hint-text hint-id="idTest" hint="This is a test hint"></ontario-hint-text>`,
+      });
+
+      expect(page.root).toMatchSnapshot();
+    });
+  })
+
   it('renders', async () => {
     const page = await newSpecPage({
       components: [OntarioHintText],
