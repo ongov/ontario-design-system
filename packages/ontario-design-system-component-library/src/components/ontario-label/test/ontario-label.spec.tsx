337d071bed1244525387ccedb11d271dff63a76a
Elsa Ng <elsa.ng@ontario.ca>
2022-01-13 09:32:58 -0500

    Add test snapshot

--- packages/ontario-design-system-component-library/src/components/ontario-label/test/ontario-label.spec.tsx
+++ packages/ontario-design-system-component-library/src/components/ontario-label/test/ontario-label.spec.tsx
@@ -1,17 +1,31 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { OntarioLabel } from '../ontario-label';
 
-describe('ontario-label', () => {
-  it('renders', async () => {
+describe('ontario-label snapshot', () => {
+  it('should render the expected html of ontario-label', async () => {
     const page = await newSpecPage({
       components: [OntarioLabel],
-      html: `<ontario-label></ontario-label>`,
+      html: `<ontario-label>Default Label</ontario-label>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+})
+
+describe('ontario-label', () => {
+  it('should render the default label element', async () => {
+    const page = await newSpecPage({
+      components: [OntarioLabel],
+      html: `<ontario-label>Default Label</ontario-label>`,
     });
     expect(page.root).toEqualHtml(`
       <ontario-label>
         <mock:shadow-root>
-          <slot></slot>
+          <label class="ontario-label">Default Label
+            <span class="ontario-label__flag">(optional)</span>
+          </label>
         </mock:shadow-root>
+        Default Label
       </ontario-label>
     `);
   });
