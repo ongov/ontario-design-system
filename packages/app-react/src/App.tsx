565537372db71ca99bebe57df0a2972c87085184
Erin Carrieres <erin.carrieres@ontario.ca>
2022-01-24 16:22:24 -0500

    remove ontario-icon-account

--- packages/app-react/src/App.tsx
+++ packages/app-react/src/App.tsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import '../../ontario-design-system-component-library/dist/ontario-design-system-components/ontario-icon-account.entry';
-import { Ontario_icon_account } from '../../ontario-design-system-component-library/dist/ontario-design-system-components/ontario-icon-account.entry';
-
 
 interface State {
   value: number | null;
@@ -28,7 +26,6 @@ class App extends Component<{}, State> {
       <div className="App">
         <div className="App-content">
           <p>This is a sample React App where we can build out a demo of the Web Components.</p>
-          <Ontario_icon_account color="blue"></Ontario_icon_account>
         </div>
       </div>
     );
