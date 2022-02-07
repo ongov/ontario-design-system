d02056accef308210c414fe9cabdc738db0736ab
Elsa Ng <elsa.ng@ontario.ca>
2022-02-07 11:56:37 -0500

    Update import

--- packages/ontario-design-system-component-library-react/src/components.ts
+++ packages/ontario-design-system-component-library-react/src/components.ts
@@ -3,9 +3,9 @@
 /* auto-generated react proxies */
 import { createReactComponent } from './react-component-lib';
 
-import type { JSX } from '@ontario-digital-service/ontario-design-system-component-library';
+import type { JSX } from 'ontario-design-system-component-library';
 
-import { defineCustomElements } from '@ontario-digital-service/ontario-design-system-component-library/loader';
+import { defineCustomElements } from 'ontario-design-system-component-library/loader';
 
 defineCustomElements();
 export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
