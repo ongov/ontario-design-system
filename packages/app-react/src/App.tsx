98f47638a9fa20f1e4c75bb5355449dac196ed17
Alex Tsarapkine <alex.tsarapkine@ontario.ca>
2022-02-15 16:13:37 -0500

    added footer

--- packages/app-react/src/App.tsx
+++ packages/app-react/src/App.tsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './App.scss';
 import StoryRouter from './Story';
 import { BrowserRouter } from 'react-router-dom';
-import Header from './Prototype Components/Header';
-import Footer from './Prototype Components/Footer';
+import Header from './Prototype-Components/Header';
+import Footer from './Prototype-Components/Footer';
 // import '../node_modules/ontario-design-system-global-styles/src/scss/theme.scss';
 // import '../ ontario-design-system-global-styles/src/scss/theme.scss';
 
