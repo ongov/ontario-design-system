30e0e6a66540ac4f4a2ceb06df3463cf5b62acc0
Alex Tsarapkine <alex.tsarapkine@ontario.ca>
2022-02-24 16:08:23 -0500

    changed layout and added code examples for button page

--- packages/app-react/src/stories/button-page/Button.tsx
+++ packages/app-react/src/stories/button-page/Button.tsx
@@ -1,22 +1,31 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState } from 'react';
 import './Button.scss';
 import { OntarioButton } from '@ontario-digital-service/ontario-design-system-component-library-react';
 
 const StoryOntarioButton = () => {
-	const iframeref = useRef<HTMLIFrameElement>(null);
-	useEffect(() => {
-		if (iframeref.current) {
-			const current = iframeref.current;
-			const header = current.contentWindow?.document.getElementById('header');
-			if (header) {
-				header.style.display = 'none';
-			}
-			const footer = current.contentWindow?.document.getElementById('footer');
-			if (footer) {
-				footer.style.display = 'none';
-			}
-		}
-	}, [iframeref]);
+	// const iframeref = useRef<HTMLIFrameElement>(null);
+	// useEffect(() => {
+	// 	if (iframeref.current) {
+	// 		const current = iframeref.current;
+	// 		const header = current.contentWindow?.document.getElementById('header');
+	// 		if (header) {
+	// 			header.style.display = 'none';
+	// 		}
+	// 		const footer = current.contentWindow?.document.getElementById('footer');
+	// 		if (footer) {
+	// 			footer.style.display = 'none';
+	// 		}
+	// 	}
+	// }, [iframeref]);
+
+	const primaryButtonCodeExample = `import { OntarioButton } from '@ontario-digital-service/ontario-design-system-component-library-react';
+	<OntarioButton type="primary">Click Me!</OntarioButton>`;
+
+	const secondaryButtonCodeExample = `import { OntarioButton } from '@ontario-digital-service/ontario-design-system-component-library-react';
+	<OntarioButton type="secondary">Click Me!</OntarioButton>`;
+
+	const tertiaryButtonCodeExample = `import { OntarioButton } from '@ontario-digital-service/ontario-design-system-component-library-react';
+	<OntarioButton type="tertiary">Click Me!</OntarioButton>`;
 
 	const [toggleHTML, setToggleHTML] = useState(false);
 	const [toggleSCSS, setToggleSCSS] = useState(false);
@@ -54,16 +63,15 @@ const StoryOntarioButton = () => {
 
 			<div className="ontario-row">
 				<div className="ontario-columns ontario-medium-12 ontario-large-12">
-					<div className="preview-container">
-						<div className="button-preview">
-							<h5>Primary:</h5>
-							<OntarioButton type="primary">Click Me!</OntarioButton>
-							<h5>Secondary:</h5>
-							<OntarioButton type="secondary">Click Me!</OntarioButton>
-							<h5>Tertiary:</h5>
-							<OntarioButton type="tertiary">Click Me!</OntarioButton>
-						</div>
-						<div className="previewNav">
+					{/* <div className="button-preview">
+						<h5>Primary:</h5>
+						<OntarioButton type="primary">Click Me!</OntarioButton>
+						<h5>Secondary:</h5>
+						<OntarioButton type="secondary">Click Me!</OntarioButton>
+						<h5>Tertiary:</h5>
+						<OntarioButton type="tertiary">Click Me!</OntarioButton>
+					</div> */}
+					{/* <div className="previewNav">
 							{toggleHTML ? (
 								<button className="on" onClick={handleToggleHTML}>
 									HTML
@@ -97,20 +105,98 @@ const StoryOntarioButton = () => {
 							{toggleHTML ? <p className="previewNavContent--HTML"> HTML ahdashjdasdjasd</p> : <></>}
 							{toggleSCSS ? <p className="previewNavContent--SCSS">SCSS asdasdasdasda</p> : <></>}
 							{toggleCSS ? <p className="previewNavContent--C-CSS">CSS sajdashdjkasdhjkas</p> : <></>}
-						</div>
-					</div>
+						</div> */}
 				</div>
 			</div>
 
 			<div className="ontario-row">
 				<div className="ontario-columns ontario-medium-12 ontario-large-12">
 					<h2>When to use this component</h2>
-					Use buttons when you want the user to do something (often called a “call to action”), for example: start an application process or a transaction agree to make a payment
-					download a PDF Don’t use a button when you’re not encouraging an action. Buttons should not be used the same way as links, which usually send the user to another page or
-					to a different part of the same page. Types of buttons Use a primary button to draw attention to the main action you want to help the user take. Avoid using multiple
-					primary buttons on one page unless the page has equally important calls to action. Use a secondary button for a less important call to action on a page. Avoid using
-					multiple secondary buttons so the user is not confused about what they should do next. Instead, ask the writer to simplify or break up the content so that it doesn’t need
-					multiple secondary buttons. Use tertiary buttons for links that should function like a butto
+					<p>
+						{' '}
+						Use buttons when you want the user to <strong>do</strong> something (often called a “call to action”), for example:
+					</p>
+
+					<ul>
+						<li>
+							<p>start an application process or a transaction</p>
+						</li>
+						<li>
+							<p>agree to make a payment</p>
+						</li>
+						<li>
+							<p>download a PDF</p>
+						</li>
+					</ul>
+					<p>
+						Don’t use a button when you’re not encouraging an action. Buttons <strong>should not be used the same way as links</strong>, which usually send the user to another page
+						or to a different part of the same page.
+					</p>
+
+					<hr />
+					<h2>Types of buttons</h2>
+					<p>
+						Use a <strong>primary button</strong> to draw attention to the <strong>main action</strong> you want to help the user take. Avoid using multiple primary buttons on one
+						page unless the page has equally important calls to action.
+					</p>
+
+					<div className="button-preview">
+						<h5>Primary:</h5>
+						<OntarioButton type="primary">Click Me!</OntarioButton>
+					</div>
+
+					<p>With the following markup:</p>
+
+					<div className="code-preview">
+						<code>
+							<pre>{primaryButtonCodeExample}</pre>
+						</code>
+					</div>
+
+					<p>
+						Use a <strong>secondary button</strong> for a <strong>less important</strong> call to action on a page. Avoid using multiple secondary buttons so the user is not
+						confused about what they should do next. Instead, ask the writer to simplify or break up the content so that it doesn’t need multiple secondary buttons.
+					</p>
+					<div className="button-preview">
+						<h5>Secondary:</h5>
+						<OntarioButton type="secondary">Click Me!</OntarioButton>
+					</div>
+
+					<p>With the following markup:</p>
+
+					<div className="code-preview">
+						<code>
+							<pre>{secondaryButtonCodeExample}</pre>
+						</code>
+					</div>
+
+					<p>
+						Use <strong>tertiary buttons</strong> for links that should function like a button, such as “edit” or “cancel” in applications. It’s okay to use more than one tertiary
+						button on a page.
+					</p>
+					<div className="button-preview">
+						<h5>Tertiary:</h5>
+						<OntarioButton type="tertiary">Click Me!</OntarioButton>
+					</div>
+
+					<p>With the following markup:</p>
+
+					<div className="code-preview">
+						<code>
+							<pre>{tertiaryButtonCodeExample}</pre>
+						</code>
+					</div>
+
+					<h3>Disabled buttons</h3>
+					<p>
+						Disabling buttons until users have correctly completed a form can cause usability and accessibility problems. Disabled buttons can create barriers for assistive
+						technology users and users with physical or cognitive impairments, and they can be confusing and frustrating for all users because they do not help the user understand
+						what they need to do to proceed.
+					</p>
+					<p>
+						A better approach is to <strong></strong>keep buttons enabled and instead <strong></strong>develop error handling to provide clear feedback about any missed fields or
+						input errors when the user tries to submit the form. For guidance on error messaging, see Page alerts.
+					</p>
 				</div>
 			</div>
 		</div>
