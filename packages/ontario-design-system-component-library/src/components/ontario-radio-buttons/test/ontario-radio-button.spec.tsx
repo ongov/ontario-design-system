da462b0ec9cecb997e7fc11d27feefd8ef5841f0
Erin Carrieres <erin.carrieres@ontario.ca>
2022-02-25 12:24:34 -0500

    comment out tests

--- packages/ontario-design-system-component-library/src/components/ontario-radio-buttons/test/ontario-radio-button.spec.tsx
+++ packages/ontario-design-system-component-library/src/components/ontario-radio-buttons/test/ontario-radio-button.spec.tsx
@@ -1,18 +1,18 @@
-// import { newSpecPage } from '@stencil/core/testing';
-// import { OntarioRadioButtons } from '../ontario-radio-buttons';
+import { newSpecPage } from '@stencil/core/testing';
+import { OntarioRadioButtons } from '../ontario-radio-buttons';
 
-// describe('ontario-radio-buttons', () => {
-//   it('renders', async () => {
-//     const page = await newSpecPage({
-//       components: [OntarioRadioButtons],
-//       html: `<ontario-radio-buttons></ontario-radio-buttons>`,
-//     });
-//     expect(page.root).toEqualHtml(`
-//       <ontario-radio-buttons>
-//         <mock:shadow-root>
-//           <slot></slot>
-//         </mock:shadow-root>
-//       </ontario-radio-buttons>
-//     `);
-//   });
-// });
+describe('ontario-radio-buttons', () => {
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [OntarioRadioButtons],
+      html: `<ontario-radio-buttons></ontario-radio-buttons>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <ontario-radio-buttons>
+        <mock:shadow-root>
+          <slot></slot>
+        </mock:shadow-root>
+      </ontario-radio-buttons>
+    `);
+  });
+});
