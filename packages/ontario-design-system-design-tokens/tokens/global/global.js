7af1a8c5e042c49c53f1527c0712cf32f0afc846
Erin Carrieres <erin.carrieres@ontario.ca>
2022-03-02 08:26:23 -0500

    updated some variables

--- packages/ontario-design-system-design-tokens/tokens/global/global.js
+++ packages/ontario-design-system-design-tokens/tokens/global/global.js
@@ -3,6 +3,7 @@ module.exports = {
     radius: { value: "4px" },
     pixelValue: { value: "0.063em" },
     maxValue: { value: "999999999999" },
+    narrowWidth: { value: "38.75em" },
     standardWidth: { value: "48rem" },
     fullWidth: { value: "100%" },
     borderSize: { value: "2px" },
