fc432d22510bc3a30b2111a9ed1881610c0ef9aa
Jeyoung Jung <jeyoung.jung@ontario.ca>
2022-03-15 12:38:27 -0400

    app.tsx update

--- packages/app-react/src/App.tsx
+++ packages/app-react/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import '../../ontario-design-system-component-library/dist/ontario-design-system-components/ontario-icon-account.entry';
-import { OntarioHeader } from '@ontario-digital-service/ontario-design-system-component-library-react';
 
 interface State {
   value: number | null;
@@ -26,7 +25,6 @@ class App extends Component<{}, State> {
     return (
       <div className="App">
         <div className="App-content">
-			<OntarioHeader/>
           <p>This is a sample React App where we can build out a demo of the Web Components.</p>
         </div>
       </div>
