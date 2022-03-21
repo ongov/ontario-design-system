6b7742a0ef080e745e21882fa9ea91a002248c10
Jeyoung Jung <jeyoung.jung@ontario.ca>
2022-03-21 14:36:00 -0400

    thread updates

--- packages/app-react/src/Prototype-Components/ontario-callouts-and-asides/ontario-callouts.tsx
+++ packages/app-react/src/Prototype-Components/ontario-callouts-and-asides/ontario-callouts.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './ontario-callouts.scss';
 
 const Callout = () => (
-	<>
-		<div className="ontario-callout">
-			<h2 className="ontario-callout__title ontario-h5">Help us improve the design system</h2>
-			<p>If you have any questions or feedback, please <a href="https://sb-designsystem.ontariogovernment.ca/alex/docs/help-feedback.html">get in touch</a>. We'd love to hear from you</p>
-		</div>
-	</>
+	<div className="ontario-callout">
+		<h2 className="ontario-callout__title ontario-h5">Help us improve the design system</h2>
+		<p>
+			If you have any questions or feedback, please <a href="https://sb-designsystem.ontariogovernment.ca/alex/docs/help-feedback.html">get in touch</a>. We'd love to hear from you
+		</p>
+	</div>
 );
 
 export default Callout;
