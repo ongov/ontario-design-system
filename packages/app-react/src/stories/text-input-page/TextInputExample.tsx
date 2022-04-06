298b3d01093e782993c8c0e2929fa36274bbf24b
Alex Tsarapkine <alex.tsarapkine@ontario.ca>
2022-04-06 14:10:08 -0400

    removed p from li in text input example

--- packages/app-react/src/stories/text-input-page/TextInputExample.tsx
+++ packages/app-react/src/stories/text-input-page/TextInputExample.tsx
@@ -47,36 +47,26 @@ export default function TextInputExample() {
 					<p>Use a text input if:</p>
 					<ul>
 						<li>
-							<p>
-								you want the user to enter <strong>no more than a single line</strong> of information
-							</p>
+							you want the user to enter <strong>no more than a single line</strong> of information
 						</li>
 						<li>
-							<p>
-								you need <strong>unique information</strong> from the user such as their name or phone number
-							</p>
+							you need <strong>unique information</strong> from the user such as their name or phone number
 						</li>
 						<li>
-							<p>
-								it’s <strong>faster for the user</strong> to write out an answer rather than choose from a list
-							</p>
+							it’s <strong>faster for the user</strong> to write out an answer rather than choose from a list
 						</li>
 					</ul>
 
 					<p>Do not use a text input if:</p>
 					<ul>
 						<li>
-							<p>
-								you want users to enter answers that are <strong>longer than one line</strong> (instead, consider a <a href="/ontario-text-area">text area</a>)
-							</p>
+							you want users to enter answers that are <strong>longer than one line</strong> (instead, consider a <a href="/ontario-text-area">text area</a>)
 						</li>
 						<li>
-							<p>
-								you want users to choose from a <strong>set list of responses</strong> such as yes or no (instead, consider{' '}
-								<a href="https://designsystem.ontario.ca/components/detail/radio-buttons.html">radio buttons</a>,{' '}
-								<a href="https://designsystem.ontario.ca/components/detail/checkboxes.html">checkboxes</a> or{' '}
-								<a href="https://designsystem.ontario.ca/components/detail/dropdown-lists.html">dropdown lists</a>)
-							</p>
+							you want users to choose from a <strong>set list of responses</strong> such as yes or no (instead, consider{' '}
+							<a href="https://designsystem.ontario.ca/components/detail/radio-buttons.html">radio buttons</a>,{' '}
+							<a href="https://designsystem.ontario.ca/components/detail/checkboxes.html">checkboxes</a> or{' '}
+							<a href="https://designsystem.ontario.ca/components/detail/dropdown-lists.html">dropdown lists</a>)
 						</li>
 					</ul>
 					<hr />
