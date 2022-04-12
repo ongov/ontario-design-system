824a5f4b613637e41889b9d7fb443608018a9098
Erin Carrieres <erin.carrieres@ontario.ca>
2022-04-12 12:01:26 -0400

    give the users the ability to pass in html content

--- packages/ontario-design-system-component-library/src/components/ontario-hint-expander/hint-expander.interface.ts
+++ packages/ontario-design-system-component-library/src/components/ontario-hint-expander/hint-expander.interface.ts
@@ -2,7 +2,9 @@ import { Hint } from '../../utils/common.interface';
 
 export interface HintExpander extends Hint {
 	/**
-	 * Content to display as the hint, once the expander is toggled open
+	 * Content to display as the hint, once the expander is toggled open.
+	 * Please note that any content that is passed into this prop will only be displayed as a string.
+	 * If you would like to add HTML content, supply child content to the component.
 	 */
 	content: string;
 
