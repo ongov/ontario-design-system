8486985a134dbe0d354486005447a90d4f696dd7
Elsa Ng <elsa.ng@ontario.ca>
2022-04-14 13:01:46 -0400

    Refine icon interface
    Icon internface has been modified to remove the `colour` property and
    the `validateColour` function to enhance implementation
    at component level.
    A new `IconWithColour` interface extending the `Icon` interface is
    exported for icon components that
    uses the `colour` property.

--- packages/ontario-design-system-component-library/src/components/ontario-icon/icon.interface.ts
+++ packages/ontario-design-system-component-library/src/components/ontario-icon/icon.interface.ts
@@ -2,37 +2,56 @@ import { IconColour } from './ontario-icon.enum';
 
 export interface Icon {
 	/**
-	 * Set the icon's colour.
-	 * Some icon components do not implement this property and hence implementation is optional.
-     * Note that the `keyof typeof` syntax is not necessary to use the enum as a type with StencilJS component.
-	 */
-	colour?: IconColour;
-
-	/**
 	 * The icon width will autogenerate the height since the icons are in square format, thus preserving
-	 * the aspect ratio. Implementation is mandatory.
+	 * the aspect ratio.
+     * Implementation is mandatory.
 	 */
 	iconWidth: number;
 
     /**
-     * Validate user input for the `colour` property.
-     * Implementation is optional because some icons don't use the `colour` property.
-     */
-    validateColour?: () => void;
+	 * Mutable variable, for internal use only.
+	 * Set the icon's width depending on validation result.
+     * Implementation is mandatory.
+	 */
+	iconWidthState: number;
 
 	/**
-	 * Validate user input for the `iconWidth` property. Implementation is mandatory.
+	 * Validate user input for the `iconWidth` property.
+     * Implementation is mandatory.
 	 */
 	validateWidth: () => void;
 
 	/**
 	 * Stencil component lifecycle method that is called once after the component is first connected to the DOM.
-     * Implementation is optional.
+	 * Implementation is optional.
 	 */
 	componentWillLoad?: () => void;
 
 	/**
-	 * Returns the HTML code to be rendered into a custom element. Implementation is mandatory.
+	 * Returns the HTML code to be rendered into a custom element.
+     * Implementation is mandatory.
 	 */
 	render?: () => object;
+}
+
+export interface IconWithColour extends Icon {
+    /**
+	 * Set the icon's colour.
+	 * Note that the `keyof typeof` syntax is not necessary to use the enum as a type with StencilJS component.
+     * Implementation is mandatory.
+	 */
+	colour: IconColour;
+
+    /**
+	 * Mutable variable, for internal use only.
+	 * Set the icon's colour based on validation result.
+     * Implementation is Mandatory
+	 */
+	iconColourState: IconColour;
+
+    /**
+	 * Validate user input for the `colour` property.
+     * Implementation is mandatory.
+	 */
+	validateColour?: () => void;
 }
