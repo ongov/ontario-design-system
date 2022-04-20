09ab9fde1f969cb28d4f61d88bf249da3dc4cc79
Scott Morris <scott.morris@ontario.ca>
2022-04-20 15:07:49 -0400

    Add support for string only messages to printConsoleMessages

--- packages/ontario-design-system-component-library/src/utils/console-message/console-message.ts
+++ packages/ontario-design-system-component-library/src/utils/console-message/console-message.ts
@@ -34,26 +34,37 @@ function addSpecifier(message: string): string {
 }
 
 // print message to console depending on the `ConsoleType`
-export function printConsoleMessage(messageObject: ConsoleMessage[], consoleType = ConsoleType.Warning, hasSystemTag = true) {
+export function printConsoleMessage(messages: ConsoleMessage[] | string, consoleType = ConsoleType.Warning, hasDesignSystemTag = true) {
 	// retrieve message from each object and concatenate them into one single string in order
 	// if `hasSystemTag` is true then the message string will being with the value stored within `designSystemTag`.
-	const message = messageObject.reduce((message: string, currentObject: ConsoleMessage) => (message += addSpecifier(currentObject.message) ?? ''), hasSystemTag ? addSpecifier(designSystemTag) : '');
+	const message =
+		typeof messages === 'string' // Note the TypeScript transpiler Stencil is using doesn't like this type guard as a const.
+			? `${addSpecifier(designSystemTag)}${addSpecifier(` ${messages}`)}`
+			: messages?.reduce(
+					(message: string, currentObject: ConsoleMessage) => (message += addSpecifier(currentObject.message) ?? ''),
+					hasDesignSystemTag ? addSpecifier(designSystemTag) : '',
+			  );
 
 	// array of arguments to be passed into the the console function
 	const messageArray: string[] = [];
 
 	// push message into the array of arguments
 	// depending on whether the system tag is used (i.e. `hasSystemTag` is true), push `tagStyles` into the array
-	if (hasSystemTag) {
+	if (hasDesignSystemTag) {
 		messageArray.push(message, tagStyles);
 	} else {
 		messageArray.push(message);
 	}
 
-	// push style of each message into the array of arguments in order
-	messageObject.forEach((message: ConsoleMessage) => {
-		messageArray.push(message.style && message.style === MessageStyle.Code ? codedTextStyles : regularTextStyles);
-	});
+	// Note the TypeScript transpiler Stencil is using doesn't like this type guard as a const.
+	if (typeof messages === 'string') {
+		messageArray.push(regularTextStyles);
+	} else {
+		// push style of each message into the array of arguments in order
+		messages?.forEach((message: ConsoleMessage) => {
+			messageArray.push(message.style && message.style === MessageStyle.Code ? codedTextStyles : regularTextStyles);
+		});
+	}
 
 	// pass array of arguments into the console function for printing depending on `consoleType`
 	// the `function.apply()` function handles array of arguments which allows list of arguments to be set programmatically
