33045a881865d7ff4e47758a3b9afd1f1cea7840
Elsa Ng <elsa.ng@ontario.ca>
2022-04-25 16:01:13 -0400

    Move `getWarningMessage()` to `input-caption.ts`

--- packages/ontario-design-system-component-library/src/utils/input-caption/input-caption.tsx
+++ packages/ontario-design-system-component-library/src/utils/input-caption/input-caption.tsx
@@ -1,9 +1,9 @@
 import { h } from '@stencil/core';
 import { CaptionType, MessageContentType } from './input-caption.enum';
 import { Caption } from './caption.interface';
-import { ConsoleType } from '../../utils/console-message/console-message.enum';
+import { ConsoleMessage } from '../console-message/console-message.interface';
+import { ConsoleType, MessageStyle } from '../../utils/console-message/console-message.enum';
 import { printConsoleMessage } from '../../utils/console-message/console-message';
-import { getWarningMessage } from './caption-message';
 
 export class InputCaption implements Caption {
 	/**
@@ -39,7 +39,7 @@ export class InputCaption implements Caption {
 	 */
 	constructor(componentTagName: string, caption: InputCaption | string) {
 		let captionObject = new Object() as InputCaption;
-		
+
 		if (caption) {
 			if (typeof caption === 'string') {
 				captionObject = JSON.parse(caption) as InputCaption;
@@ -52,7 +52,8 @@ export class InputCaption implements Caption {
 		this.isLegend = captionObject?.isLegend ?? false;
 		this.componentTagName = componentTagName;
 		this.captionText = captionObject?.captionText ?? '';
-		this.captionType = (captionObject && captionObject?.captionType && Object.values(CaptionType).find(type => type === captionObject?.captionType?.toLowerCase())) || CaptionType.Default;
+		this.captionType =
+			(captionObject && captionObject?.captionType && Object.values(CaptionType).find(type => type === captionObject?.captionType?.toLowerCase())) || CaptionType.Default;
 		this.validateCaption(captionObject);
 	}
 
@@ -113,27 +114,146 @@ export class InputCaption implements Caption {
 	private validateCaption(caption?: InputCaption) {
 		// undefined `caption` object
 		if (!caption || Object.keys(caption).length <= 0) {
-			printConsoleMessage(getWarningMessage(MessageContentType.UndefinedCaptionObject, this.componentTagName), ConsoleType.Warning);
+			printConsoleMessage(this.getWarningMessage(MessageContentType.UndefinedCaptionObject, this.componentTagName), ConsoleType.Warning);
 		} else {
 			// undefined `captionText` property
 			if (!caption.captionText) {
-				printConsoleMessage(getWarningMessage(MessageContentType.UndefinedCaptionText, this.componentTagName, this.getRequiredFlagText()), ConsoleType.Warning);
-			}  else {
+				printConsoleMessage(this.getWarningMessage(MessageContentType.UndefinedCaptionText, this.componentTagName, this.getRequiredFlagText()), ConsoleType.Warning);
+			} else {
 				// `captionText` that is empty or contains only spaces
 				if (/^\s*$/.test(caption.captionText)) {
-					printConsoleMessage(getWarningMessage(MessageContentType.EmptyCaptionText, this.componentTagName ,this.getRequiredFlagText()), ConsoleType.Warning);
+					printConsoleMessage(this.getWarningMessage(MessageContentType.EmptyCaptionText, this.componentTagName, this.getRequiredFlagText()), ConsoleType.Warning);
 				}
 			}
 
-			// undefined `captionType` 
+			// undefined `captionType`
 			if (!caption.captionType) {
-				printConsoleMessage(getWarningMessage(MessageContentType.UndefinedCaptionType, this.componentTagName), ConsoleType.Warning);
+				printConsoleMessage(this.getWarningMessage(MessageContentType.UndefinedCaptionType, this.componentTagName), ConsoleType.Warning);
 			} else {
 				// incorrect `captionType`
 				if (!Object.values(CaptionType).includes(caption?.captionType?.toLowerCase() as CaptionType)) {
-					printConsoleMessage(getWarningMessage(MessageContentType.IncorrectCaptionType, this.componentTagName), ConsoleType.Warning);
+					printConsoleMessage(this.getWarningMessage(MessageContentType.IncorrectCaptionType, this.componentTagName), ConsoleType.Warning);
 				}
 			}
 		}
+	}
+
+	/**
+	 * Generate the content of warning message to be printed to the console
+	 * @param messageType determine the content of warning message to output
+	 * @param componentTagName the tag name of the component that's emitting the warning message
+	 * @param requiredFlagText accepts a string value to be used as the required flag text and defaults to `(optional)` if not set
+	 * @returns an array of `ConsoleMessage` objects containing the message and associated styles to be printed to the console
+	 */
+	private getWarningMessage(messageType: MessageContentType, componentTagName: string, requiredFlagText: string = '(optional)'): ConsoleMessage[] {
+		let message: ConsoleMessage[] = [
+			{
+				message: ' caption ',
+				style: MessageStyle.Code,
+			},
+			{
+				message: 'object on',
+				style: MessageStyle.Regular,
+			},
+			{
+				message: ` <${componentTagName}> `,
+				style: MessageStyle.Code,
+			},
+		];
+
+		if (messageType !== MessageContentType.UndefinedCaptionObject) {
+			const problematicProperty: ConsoleMessage[] = [
+				{
+					message: ` ${messageType === MessageContentType.EmptyCaptionText || messageType === MessageContentType.UndefinedCaptionText ? 'captionText' : 'captionType'} `,
+					style: MessageStyle.Code,
+				},
+				{
+					message: 'property of',
+					style: MessageStyle.Regular,
+				},
+			];
+			message = [...problematicProperty, ...message];
+		}
+
+		switch (messageType) {
+			// undefinedCaptionObject example: caption object on <ontario-input> is required but not defined. A blank followed by a (optional) flag is assumed.
+			// undefinedCaptionText example: captionText property of caption object on <ontario-input> is required but not defined. A blank followed by a (optional) flag is assumed.
+			// EmptyCaptionText example: captionText property of caption object on <ontario-input> is empty or contains only spaces. A blank followed by a (optional) flag is assumed.
+			case MessageContentType.UndefinedCaptionObject:
+			case MessageContentType.UndefinedCaptionText:
+			case MessageContentType.EmptyCaptionText:
+				let undefinedMessage: ConsoleMessage[] = [
+					{
+						message: `${messageType === MessageContentType.EmptyCaptionText ? 'is empty or contains only spaces' : 'is required but not defined'}. A blank followed by a`,
+						style: MessageStyle.Regular,
+					},
+					{
+						message: ` ${requiredFlagText} `,
+						style: MessageStyle.Code,
+					},
+					{
+						message: 'flag is assumed.',
+						style: MessageStyle.Regular,
+					},
+				];
+				message = [...message, ...undefinedMessage];
+				break;
+
+			// UndefinedCaptionType example: captionType property of caption object on <ontario-input> is not defined. The default type is assumed.
+			case MessageContentType.UndefinedCaptionType:
+				let undefinedTypeMessage: ConsoleMessage[] = [
+					{
+						message: 'is not defined. The',
+						style: MessageStyle.Regular,
+					},
+					{
+						message: ` default `,
+						style: MessageStyle.Code,
+					},
+					{
+						message: 'type is assumed.',
+						style: MessageStyle.Regular,
+					},
+				];
+				message = [...message, ...undefinedTypeMessage];
+				break;
+
+			// IncorrectCaptionType example: captionType property of caption object on <ontario-input> was set to an incorrect type; only default, heading or large type is allowed. The default type is assumed.
+			case MessageContentType.IncorrectCaptionType:
+				let incorrectTypeMessage: ConsoleMessage[] = [
+					{
+						message: `was set to an incorrect type; only`,
+						style: MessageStyle.Regular,
+					},
+					{
+						message: ' default, heading, ',
+						style: MessageStyle.Code,
+					},
+					{
+						message: 'or',
+						style: MessageStyle.Regular,
+					},
+					{
+						message: ' large ',
+						style: MessageStyle.Code,
+					},
+					{
+						message: 'type is allowed. The',
+						style: MessageStyle.Regular,
+					},
+					{
+						message: ' default ',
+						style: MessageStyle.Code,
+					},
+					{
+						message: 'type is assumed.',
+						style: MessageStyle.Regular,
+					},
+				];
+				message = [...message, ...incorrectTypeMessage];
+				break;
+		}
+
+		return message;
 	}
 }
