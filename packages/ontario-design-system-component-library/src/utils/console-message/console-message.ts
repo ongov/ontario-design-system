a91ef33de67838fcc66765df26c23bdd00031585
Elsa Ng <elsa.ng@ontario.ca>
2022-05-19 10:50:24 -0400

    ConsoleMessage fluent interface POC

--- packages/ontario-design-system-component-library/src/utils/console-message/console-message.ts
+++ packages/ontario-design-system-component-library/src/utils/console-message/console-message.ts
@@ -82,3 +82,63 @@ export function printConsoleMessage(messages: ConsoleMessage[] | string, console
 			console.log.apply(null, messageArray);
 	}
 }
+
+// POC to use a fluent interface to build and print console messages
+export class consoleMessage {
+	message: string;
+	styles: string[];
+
+	constructor() {
+		this.message = '';
+		this.styles = [];
+	}
+
+	// adds the `Ontario Design System` tag to the beginning of the message
+	// the first call in the chain if a tag is required
+	addDesignSystemTag() {
+		this.message = addSpecifier(designSystemTag);
+		this.styles.push(tagStyles);
+		return this;
+	}
+
+	// builds console message as regular text 
+	addRegularText(text: string) {
+		this.addText(text, regularTextStyles);
+	}
+
+	// builds console message as monospace text
+	addMonospaceText(text: string) {
+		this.addText(text, codedTextStyles);
+	}
+
+	// prints message to console depending on the `ConsoleType`
+	// the last function call in the chain
+	printMessage(consoleType = ConsoleType.Warning) {
+		const messageArray = [this.message, ...this.styles];
+
+		// pass array of arguments into the console function for printing depending on `consoleType`
+		// the `function.apply()` function handles array of arguments which allows list of arguments to be set programmatically
+		switch (consoleType) {
+			case ConsoleType.Error:
+				console.error.apply(null, messageArray);
+				break;
+			case ConsoleType.Info:
+				console.info.apply(null, messageArray);
+				break;
+			case ConsoleType.Warning:
+				console.warn.apply(null, messageArray);
+				break;
+			default:
+				console.log.apply(null, messageArray);
+		}
+	}
+
+	// adds text to the `message` property and associated styles into the `styles` array
+	private addText(text: string, style: string) {
+		if (text && text?.trim().length > 0) {
+			this.message += addSpecifier(text);
+			this.styles.push(style);
+		}
+		return this;
+	}
+}
