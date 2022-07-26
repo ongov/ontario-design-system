c505cd48a5e5925d83f23e192cb5e3f57b6597a6
Erin Carrieres <erin.carrieres@ontario.ca>
2022-07-26 16:02:04 -0400

    update comments

--- packages/ontario-design-system-component-library/src/utils/validation/validation-functions.tsx
+++ packages/ontario-design-system-component-library/src/utils/validation/validation-functions.tsx
@@ -26,10 +26,10 @@ export function validateObjectExists(newValue: object): boolean {
 }
 
 /**
- * Validate value against arrayList
+ * Validate value against keys
  * @param value value to be compared against keys
  * @param keys of values provided by caller
- * @returns arrayKey value if a match is found or undefined if no matches are found
+ * @returns true if a match is found or false if no matches are found
  */
  export function validateValueAgainstArray<T extends string>(value: T, keys: ReadonlyArray<string>): boolean {
 	return keys.includes(value);
