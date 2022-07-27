eea37fe8e2f64e6422edf18a6b589d05ca6d44b5
Meaghan Carrieres <meaghan.carriers@ontario.ca>
2022-07-27 15:13:24 -0400

    Updates expected props that are passed to OntarioInput in react poc (the build was breaking otherwise)

--- packages/app-react/src/stories/text-input-page/TextInputExample.tsx
+++ packages/app-react/src/stories/text-input-page/TextInputExample.tsx
@@ -22,18 +22,7 @@ export default function TextInputExample() {
 				<div className="ontario-columns ontario-medium-12 ontario-large-12">
 					<h3>Examples</h3>
 					<div className="preview-container">
-						<OntarioInput
-							labelCaption="Text Input caption"
-							labelFor="this is a label for"
-							labelType="default"
-							describedBy="described by"
-							elementId="id"
-							inputWidth="4-char-width"
-							name="name"
-							required
-							type="text"
-							value="value"
-						/>
+						<OntarioInput caption="Text Input caption" elementId="react-poc-input-id" inputWidth="4-char-width" name="react-poc-input" required type="text" />
 
 						<p>With the following markup:</p>
 
