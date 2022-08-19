3f658f8449d7cd5a8c3b413fd91623c259f0e819
Meaghan Carrieres <meaghan.carriers@ontario.ca>
2022-08-19 16:25:43 -0400

    DS-1170: Adds missing design tokens
    Adds the following design tokens to the design token package:
    - font names
    - base colours (greyscale & black)
    - font line height sizes
    - font letter spacing sizes
    
    Reorganizes the global styles project:
    - Moves `text-input`, `label` and `form` into components folder
    - Creates new overrides folder for visibility classes - these should all have "important" tags as they are meant to be used for overriding any other styles
    - Adds spacing overrides file (these are documented in the design system guidance under spacing but were not previously available)

--- packages/ontario-design-system-design-tokens/tokens/sizes/font-sizes.js
+++ packages/ontario-design-system-design-tokens/tokens/sizes/font-sizes.js
@@ -1,8 +1,26 @@
 module.exports = {
-  ontarioFontSizes: {
-    small: { value: "0.875rem" },
-    standardBodyText: { value: "1rem" },
-    large: { value: "1.25rem" },
-    xLarge: { value: "1.5rem" }
-  }
-}
+	ontarioFontSizes: {
+		small: { value: '0.875rem' },
+		standardBodyText: { value: '1rem' },
+		large: { value: '1.25rem' },
+		xLarge: { value: '1.5rem' },
+	},
+	ontarioFontLineHeights: {
+		defaultLineHeight: 1.6,
+		lineHeight1: 1.2,
+		lineHeight12: 1.29,
+		lineHeight13: 1.33,
+		lineHeight14: 1.37,
+		lineHeight15: 1.39,
+		lineHeight16: 1.43,
+		lineHeight17: 1.5,
+		lineHeight18: 1.56,
+	},
+	ontarioFontLetterSpacing: {
+		letterSpacingXSmall: '0.02rem',
+		letterSpacingSmall: '0.025rem',
+		letterSpacingMedium: '0.03rem',
+		letterSpacingLarge: '0.0313rem',
+		letterSpacingXLarge: '0.04rem',
+	},
+};
