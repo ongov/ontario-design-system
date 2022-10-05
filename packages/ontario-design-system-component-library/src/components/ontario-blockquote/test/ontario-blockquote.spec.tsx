f8a88c1b9985ef6ed665f38c726909ed96c8d86e
Meaghan Carrieres <meaghan.carriers@ontario.ca>
2022-10-05 11:55:07 -0400

    DS-1228: Develop Blockquote component
    * Adds new `ontario-blockquote` component to the component-library package with all necessary files.
    	- Adds props for quote, attribution and byline info necessary for the component
    	- Adds a `watch` method on the quote length - if the quote is above 140 characters, a new class is added to the blockquote component to control the styles.
    * Updates spec test for blockquote (further tests will be covered in DS-1229)
    * Adds `ontario-blockquote` to main index of component library package

--- packages/ontario-design-system-component-library/src/components/ontario-blockquote/test/ontario-blockquote.spec.tsx
+++ packages/ontario-design-system-component-library/src/components/ontario-blockquote/test/ontario-blockquote.spec.tsx
@@ -2,17 +2,20 @@ import { newSpecPage } from '@stencil/core/testing';
 import { OntarioBlockquote } from '../ontario-blockquote';
 
 describe('ontario-blockquote', () => {
-  it('renders', async () => {
-    const page = await newSpecPage({
-      components: [OntarioBlockquote],
-      html: `<ontario-blockquote></ontario-blockquote>`,
-    });
-    expect(page.root).toEqualHtml(`
+	it('renders', async () => {
+		const page = await newSpecPage({
+			components: [OntarioBlockquote],
+			html: `<ontario-blockquote></ontario-blockquote>`,
+		});
+		expect(page.root).toEqualHtml(`
       <ontario-blockquote>
         <mock:shadow-root>
-          <slot></slot>
+					<blockquote class="ontario-blockquote ontario-blockquote--short">
+						<p></p>
+						<cite class="ontario-blockquote__attribution"></cite>
+					</blockquote>
         </mock:shadow-root>
       </ontario-blockquote>
     `);
-  });
+	});
 });
