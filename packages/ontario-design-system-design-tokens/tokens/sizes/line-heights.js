c6415ce4f7891bbe2fefd5032f2267c6f59aae99
Erin Carrieres <erin.carrieres@ontario.ca>
2022-10-31 10:46:23 -0400

    added missing variables

---
+++ packages/ontario-design-system-design-tokens/tokens/sizes/line-heights.js
@@ -0,0 +1,13 @@
+module.exports = {
+	ontarioFontLineHeights: {
+		default: { value: '1.6' },
+		1: { value: '1.2' },
+		2: { value: '1.29' },
+		3: { value: '1.33' },
+		4: { value: '1.37' },
+		5: { value: '1.39' },
+		6: { value: '1.43' },
+		7: { value: '1.5' },
+		8: { value: '1.56' },
+	}
+};
