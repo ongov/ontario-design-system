b21c7651787e80597f36833062aad2a7e97d97ce
Erin Carrieres <erin.carrieres@ontario.ca>
2022-10-31 09:04:58 -0400

    fixed up some missing styles

--- packages/ontario-design-system-design-tokens/tokens/fonts/fonts.js
+++ packages/ontario-design-system-design-tokens/tokens/fonts/fonts.js
@@ -2,10 +2,5 @@ module.exports = {
 	ontarioFonts: {
 		openSans: '"Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
 		raleway: '"Raleway", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
-	},
-	ontarioFontWeights: {
-		normal: '400',
-		semiBold: '600',
-		bold: '700'
 	}
 };
