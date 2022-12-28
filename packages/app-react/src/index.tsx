80be2e9bc6e8eee44587d4adee78e193a5d205d7
Scott Morris <scott.morris@ontario.ca>
2022-12-28 14:23:50 -0500

    refactor(react-app): add note about the !mark
    - It is needed when using TypeScript as per the upgrade guide.

--- packages/app-react/src/index.tsx
+++ packages/app-react/src/index.tsx
@@ -7,12 +7,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root');
-const root = createRoot(container!);
+const root = createRoot(container!); // The ! is intentional **
 root.render(
 	<BrowserRouter>
 		<App />
 	</BrowserRouter>,
 );
+
+// ** The ! is needed with TypeScript
+// See the upgrade guide for reference: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
