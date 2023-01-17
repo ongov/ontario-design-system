389b894110437e89f35f744a69dfec82b239589a
Erin Carrieres <erin.carrieres@ontario.ca>
2023-01-17 11:43:30 -0500

    fix(test): resolve issues with header unit tests

--- packages/ontario-design-system-component-library/src/components.d.ts
+++ packages/ontario-design-system-component-library/src/components.d.ts
@@ -2292,7 +2292,7 @@ declare namespace LocalJSX {
         /**
           * Emitted when a keyboard input or mouse event occurs.
          */
-        "onChangeEvent"?: (event: CustomEvent<KeyboardEvent>) => void;
+        "onChangeEvent"?: (event: OntarioCheckboxesCustomEvent<KeyboardEvent>) => void;
         /**
           * Each property will be passed in through an object in the options array. This can either be passed in as an object directly (if using react), or as a string in HTML. If there are multiple checkboxes in a fieldset, each checkbox will be displayed as an option. In the example below, the options are being passed in as a string and there are two checkboxes to be displayed in the fieldset.
           * @example <ontario-checkboxes   caption='{     "captionText": "Address",     "captionType": "heading",   }   name="ontario-checkboxes",   hint-text="This is the hint text"   options='[     {        "value": "checkbox-1-value",        "label": "Checkbox Label"     },     {        "value": "checkbox-2",        "label": "checkbox-2-label",        "hintExpander": {          "hint": "Hint expander",          "content": "This is the content"        }      }   ]'   required="true" > </ontario-checkboxes>
@@ -2406,7 +2406,7 @@ declare namespace LocalJSX {
         /**
           * Emitted when a keyboard input or mouse event occurs.
          */
-        "onToggleExpanderEvent"?: (event: CustomEvent<any>) => void;
+        "onToggleExpanderEvent"?: (event: OntarioHintExpanderCustomEvent<any>) => void;
     }
     interface OntarioHintText {
         /**
@@ -3536,15 +3536,15 @@ declare namespace LocalJSX {
         /**
           * Emitted when the input loses focus.
          */
-        "onBlurEvent"?: (event: CustomEvent<void>) => void;
+        "onBlurEvent"?: (event: OntarioInputCustomEvent<void>) => void;
         /**
           * Emitted when a keyboard input occurred.
          */
-        "onChangeEvent"?: (event: CustomEvent<KeyboardEvent>) => void;
+        "onChangeEvent"?: (event: OntarioInputCustomEvent<KeyboardEvent>) => void;
         /**
           * Emitted when the input gains focus.
          */
-        "onFocusEvent"?: (event: CustomEvent<void>) => void;
+        "onFocusEvent"?: (event: OntarioInputCustomEvent<void>) => void;
         /**
           * This is used to determine whether the input is required or not. This prop also gets passed to the InputCaption utility to display either an optional or required flag in the label. If no prop is set, it will default to false (optional).
          */
@@ -3642,15 +3642,15 @@ declare namespace LocalJSX {
         /**
           * Emitted when the input loses focus.
          */
-        "onBlurEvent"?: (event: CustomEvent<void>) => void;
+        "onBlurEvent"?: (event: OntarioTextareaCustomEvent<void>) => void;
         /**
           * Emitted when a keyboard input occurred.
          */
-        "onChangeEvent"?: (event: CustomEvent<KeyboardEvent>) => void;
+        "onChangeEvent"?: (event: OntarioTextareaCustomEvent<KeyboardEvent>) => void;
         /**
           * Emitted when the input gains focus.
          */
-        "onFocusEvent"?: (event: CustomEvent<void>) => void;
+        "onFocusEvent"?: (event: OntarioTextareaCustomEvent<void>) => void;
         /**
           * This is used to determine whether the textarea is required or not. This prop also gets passed to the InputCaption utility to display either an optional or required flag in the label. If no prop is set, it will default to false (optional).
          */
@@ -3662,8 +3662,8 @@ declare namespace LocalJSX {
     }
     interface TestLanguageToggle {
         "language"?: string;
-        "onHeaderLanguageToggled"?: (event: CustomEvent<string>) => void;
-        "onSetAppLanguage"?: (event: CustomEvent<string>) => void;
+        "onHeaderLanguageToggled"?: (event: TestLanguageToggleCustomEvent<string>) => void;
+        "onSetAppLanguage"?: (event: TestLanguageToggleCustomEvent<string>) => void;
     }
     interface IntrinsicElements {
         "ontario-back-to-top": OntarioBackToTop;
