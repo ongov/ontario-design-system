588e60137e760bf5c52b7b531900d4055fab3de4
Meaghan Carrieres <meaghan.carriers@ontario.ca>
2023-02-15 17:07:42 -0500

    update critical alert tests

--- packages/ontario-design-system-component-library/src/components/ontario-critical-alert/test/ontario-critical-alert.spec.tsx
+++ packages/ontario-design-system-component-library/src/components/ontario-critical-alert/test/ontario-critical-alert.spec.tsx
@@ -14,13 +14,7 @@ describe('ontario-critical-alert', () => {
 						<div class="ontario-row">
 							<div class="ontario-column ontario-small-12">
 								<div class="ontario-critical-alert__body">
-									<div class="ontario-critical-alert__icon">
-										<svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-											<path d="M1 21H23L12 2L1 21ZM13 18H11V16H13V18ZM13 14H11V10H13V14Z" fill="white"></path>
-											<rect fill="black" height="4" width="2" x="11" y="10"></rect>
-											<rect fill="black" height="2" width="2" x="11" y="16"></rect>
-										</svg>
-									</div>
+									<div class="ontario-critical-alert__icon"></div>
 									<p>
 										This is the critical alert content
 									</p>
