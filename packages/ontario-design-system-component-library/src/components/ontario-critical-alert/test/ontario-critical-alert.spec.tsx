d60caee189554b0819fec9e873526de6d01efd05
Meaghan Carrieres <meaghan.carriers@ontario.ca>
2023-02-16 14:20:01 -0500

    adds tests for critical alert component

--- packages/ontario-design-system-component-library/src/components/ontario-critical-alert/test/ontario-critical-alert.spec.tsx
+++ packages/ontario-design-system-component-library/src/components/ontario-critical-alert/test/ontario-critical-alert.spec.tsx
@@ -26,4 +26,30 @@ describe('ontario-critical-alert', () => {
 			</ontario-critical-alert>
     `);
 	});
+
+	it('should render a critical alert component with the content property overriding the element content', async () => {
+		const page = await newSpecPage({
+			components: [OntarioCriticalAlert],
+			html: `<ontario-critical-alert content="I am the critical alert content that will display">Critical alert content</ontario-critical-alert>`,
+		});
+		expect(page.root).toEqualHtml(`
+			<ontario-critical-alert content="I am the critical alert content that will display">
+				<mock:shadow-root>
+					<div class="ontario-critical-alert">
+						<div class="ontario-row">
+							<div class="ontario-column ontario-small-12">
+								<div class="ontario-critical-alert__body">
+									<div class="ontario-critical-alert__icon"></div>
+									<p>
+										I am the critical alert content that will display
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</mock:shadow-root>
+				Critical alert content
+			</ontario-critical-alert>
+		`);
+	});
 });
