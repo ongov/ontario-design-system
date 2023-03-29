e2e614b9ea9489522c3fd746e0fba40c72881784
Meaghan Carrieres <meaghancarrieres@Meaghans-MBP.ht.home>
2023-03-29 15:28:17 -0400

    feat: adding tests

--- packages/ontario-design-system-component-library/src/components/ontario-back-to-top/test/ontario-back-to-top.e2e.ts
+++ packages/ontario-design-system-component-library/src/components/ontario-back-to-top/test/ontario-back-to-top.e2e.ts
@@ -17,6 +17,10 @@ describe('ontario-back-to-top', () => {
 		const page = await newE2EPage();
 		await page.setContent(mockBTTContent);
 
+		// scroll down the page
+
+		// this method was taken from: https://github.com/microsoft/playwright/issues/4302#issuecomment-1132919529
+		// setting delay function and calling it after setting the scrollTo was the only way to successfully get the scroll to work
 		await page.evaluate(async () => {
 			const delay = (miliseconds: number) => new Promise((resolve) => setTimeout(resolve, miliseconds));
 			window.scrollTo(0, 500);
@@ -30,21 +34,54 @@ describe('ontario-back-to-top', () => {
 		expect(styles?.visibility).toBe('visible');
 	});
 
-	it('should trigger the window to scroll back to the top of the page when clicked', async () => {
+	// These tests won't work!!
+	xit('should trigger the window to scroll back to the top of the page when clicked', async () => {
 		const page = await newE2EPage();
 		await page.setContent(mockBTTContent);
+		const component = await page.find('ontario-back-to-top');
+		const element = await page.find('ontario-back-to-top >>> button');
 
+		// scroll down the page
 		await page.evaluate(async () => {
 			const delay = (miliseconds: number) => new Promise((resolve) => setTimeout(resolve, miliseconds));
 			window.scrollTo(0, 500);
-
 			await delay(100);
 		});
 
-		await page.click('ontario-back-to-top');
+		// Simulate a click on the element
+		await component.click();
+		// await element.click();
+
 		await page.waitForChanges();
 
-		const element = await page.find('ontario-back-to-top >>> button');
 		console.log(element.className);
+
+		////////////////////////
+		//// Other attempts ////
+		///////////////////////
+
+		// const element = await page.evaluateHandle(async (mockBTTContent) => {
+		// 	const el = document.createElement('div');
+		// 	el.innerHTML = mockBTTContent;
+
+		// 	const delay = (miliseconds: number) => new Promise((resolve) => setTimeout(resolve, miliseconds));
+		// 	window.scrollTo(0, 500);
+		// 	await delay(100);
+
+		// 	return el;
+		// }, mockBTTContent);
+
+		// const component = element.$eval('ontario-back-to-top', (el) => el);
+
+		// await page.evaluate(async (component) => {
+		// 	(await component).click();
+		// }, component);
+
+		// const classNames = await page.evaluate(
+		// 	(el) => el.shadowRoot?.querySelector('button')?.textContent,
+		// 	element
+		// );
+
+		// console.log(classNames)
 	});
 });
