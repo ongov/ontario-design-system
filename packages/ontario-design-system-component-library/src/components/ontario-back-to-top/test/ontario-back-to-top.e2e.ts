29e6c1ddc16bf3d6350889371d5115cceb53bb48
Meaghan Carrieres <meaghancarrieres@Meaghans-MBP.ht.home>
2023-03-29 11:20:12 -0400

    test: wip

--- packages/ontario-design-system-component-library/src/components/ontario-back-to-top/test/ontario-back-to-top.e2e.ts
+++ packages/ontario-design-system-component-library/src/components/ontario-back-to-top/test/ontario-back-to-top.e2e.ts
@@ -1,4 +1,4 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { E2EElement, newE2EPage } from '@stencil/core/testing';
 import { mockBTTContent } from './mock-page-content';
 
 describe('ontario-back-to-top', () => {
@@ -17,16 +17,34 @@ describe('ontario-back-to-top', () => {
 		const page = await newE2EPage();
 		await page.setContent(mockBTTContent);
 
-		await page.evaluate(() => {
+		await page.evaluate(async () => {
+			const delay = (miliseconds: number) => new Promise((resolve) => setTimeout(resolve, miliseconds));
 			window.scrollTo(0, 500);
+			await delay(100);
 		});
 
 		const element = await page.find('ontario-back-to-top >>> button');
-		// const styles = element.getComputedStyle();
-		console.log(element.className);
+		const styles = await element.getComputedStyle();
 
-		// tests below don't work!
-		// expect(element).toHaveClasses(['ontario-back-top-top', 'active']);
-		// expect((await styles)?.visibility).toBe('visible');
+		expect(element.className).toEqual('ontario-back-to-top active');
+		expect(styles?.visibility).toBe('visible');
+	});
+
+	it('should trigger the window to scroll back to the top of the page when clicked', async () => {
+		const page = await newE2EPage();
+		await page.setContent(mockBTTContent);
+
+		await page.evaluate(async () => {
+			const delay = (miliseconds: number) => new Promise((resolve) => setTimeout(resolve, miliseconds));
+			window.scrollTo(0, 500);
+
+			await delay(100);
+		});
+
+		await page.click('ontario-back-to-top');
+		await page.waitForChanges();
+
+		const element = await page.find('ontario-back-to-top >>> button');
+		console.log(element.className);
 	});
 });
