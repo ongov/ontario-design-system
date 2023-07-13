b7629cca450711728e4422b7a2f6db82abc62d9a
Meaghan Carrieres <meaghancarrieres@Meaghans-MBP.ht.home>
2023-07-13 15:48:42 -0400

    feat: adds table component to web component library

---
+++ packages/ontario-design-system-component-library/src/components/ontario-table/test/ontario-table.e2e.ts
@@ -0,0 +1,11 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('ontario-table', () => {
+	it('renders', async () => {
+		const page = await newE2EPage();
+		await page.setContent('<ontario-table></ontario-table>');
+
+		const element = await page.find('ontario-table');
+		expect(element).toHaveClass('hydrated');
+	});
+});
