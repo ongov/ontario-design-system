f4f90613fd0c0e36af01182dca4aa36588664e81
Erin Carrieres <erin.carrieres@ontario.ca>
2024-01-05 15:15:31 -0500

    chore: rebasing

--- packages/ontario-design-system-component-library/src/components/ontario-card/test/ontario-cards.e2e.ts
+++ packages/ontario-design-system-component-library/src/components/ontario-card/test/ontario-cards.e2e.ts
@@ -1,11 +1,11 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-describe('ontario-cards', () => {
+describe('ontario-card', () => {
 	it('renders', async () => {
 		const page = await newE2EPage();
-		await page.setContent('<ontario-cards></ontario-cards>');
+		await page.setContent('<ontario-card></ontario-card>');
 
-		const element = await page.find('ontario-cards');
+		const element = await page.find('ontario-card');
 		expect(element).toHaveClass('hydrated');
 	});
 });
--- packages/ontario-design-system-component-library/src/components/ontario-card/test/ontario-cards.spec.tsx
+++ packages/ontario-design-system-component-library/src/components/ontario-card/test/ontario-cards.spec.tsx
@@ -1,18 +1,18 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { OntarioCard } from '../ontario-card';
 
-describe('ontario-cards', () => {
+describe('ontario-card', () => {
 	it('renders', async () => {
 		const page = await newSpecPage({
 			components: [OntarioCard],
-			html: `<ontario-cards></ontario-cards>`,
+			html: `<ontario-card></ontario-card>`,
 		});
 		expect(page.root).toEqualHtml(`
-      <ontario-cards>
+      <ontario-card>
         <mock:shadow-root>
           <slot></slot>
         </mock:shadow-root>
-      </ontario-cards>
+      </ontario-card>
     `);
 	});
 });
