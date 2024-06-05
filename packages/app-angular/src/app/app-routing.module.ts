9715eda6c4f383da921ab41025122c9e6214a525
Erin Carrieres <erin.carrieres@ontario.ca>
2024-06-05 10:23:30 -0400

    feat: update angular page titles

--- packages/app-angular/src/app/app-routing.module.ts
+++ packages/app-angular/src/app/app-routing.module.ts
@@ -10,25 +10,74 @@ import { FrameSixComponent } from './pages/framesix/framesix.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'get-started', pathMatch: 'full' },
-	{ path: 'get-started', component: FrameOneComponent },
-	{ path: 'create-account', component: FrameTwoComponent },
-	{ path: 'contact-information', component: FrameThreeComponent },
-	{ path: 'describe-role', component: FrameFourComponent },
-	{ path: 'additional-details', component: FrameFiveComponent },
-	{ path: 'account-creation', component: FrameSixComponent },
-	{ path: 'fr/demarrer', component: FrameOneComponent },
-	{ path: 'fr/creer-compte', component: FrameTwoComponent },
-	{ path: 'fr/coordonnees', component: FrameThreeComponent },
-	{ path: 'fr/decrivez-role', component: FrameFourComponent },
-	{ path: 'fr/details-supplementaires', component: FrameFiveComponent },
-	{ path: 'fr/creation-de-compte', component: FrameSixComponent },
+	{
+		path: 'get-started',
+		component: FrameOneComponent,
+		data: { title: 'Get Started | Ontario Design System Angular demo' },
+	},
+	{
+		path: 'create-account',
+		component: FrameTwoComponent,
+		data: { title: 'Create an account | Ontario Design System Angular demo' },
+	},
+	{
+		path: 'contact-information',
+		component: FrameThreeComponent,
+		data: { title: 'Contact Information | Ontario Design System Angular demo' },
+	},
+	{
+		path: 'describe-role',
+		component: FrameFourComponent,
+		data: { title: 'Describe Role | Ontario Design System Angular demo' },
+	},
+	{
+		path: 'additional-details',
+		component: FrameFiveComponent,
+		data: { title: 'Additional Details | Ontario Design System Angular demo' },
+	},
+	{
+		path: 'account-creation',
+		component: FrameSixComponent,
+		data: { title: 'Account Creation | Ontario Design System Angular demo' },
+	},
+	{
+		path: 'fr/demarrer',
+		component: FrameOneComponent,
+		data: { title: 'Démarrer | Ontario Design System Angular demo' },
+	},
+	{
+		path: 'fr/creer-compte',
+		component: FrameTwoComponent,
+		data: { title: 'Créer Compte | Ontario Design System Angular demo' },
+	},
+	{
+		path: 'fr/coordonnees',
+		component: FrameThreeComponent,
+		data: { title: 'Coordonnées | Ontario Design System Angular demo' },
+	},
+	{
+		path: 'fr/decrivez-role',
+		component: FrameFourComponent,
+		data: { title: 'Décrivez Rôle | Ontario Design System Angular demo' },
+	},
+	{
+		path: 'fr/details-supplementaires',
+		component: FrameFiveComponent,
+		data: { title: 'Détails Supplémentaires | Ontario Design System Angular demo' },
+	},
+	{
+		path: 'fr/creation-de-compte',
+		component: FrameSixComponent,
+		data: { title: 'Création de Compte | Ontario Design System Angular demo' },
+	},
 	{ path: 'fr/:**', redirectTo: 'fr/demarrer' },
 	{ path: '**', redirectTo: 'get-started' },
 ];
+
 @NgModule({
 	declarations: [],
 	imports: [CommonModule, RouterModule.forRoot(routes, { useHash: true })],
 	exports: [RouterModule],
-	providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }], // Provide the HashLocationStrategy
+	providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
 })
 export class AppRoutingModule {}
