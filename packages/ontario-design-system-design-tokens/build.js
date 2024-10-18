72c4ce15bfb341434e367c0d3ace988b894d5340
Matt St Jean <matt.m.stjean@gmail.com>
2024-10-18 10:09:19 -0400

    chore(ontario-design-system-component-library): update leftover outdated dependencies

--- packages/ontario-design-system-design-tokens/build.js
+++ packages/ontario-design-system-design-tokens/build.js
@@ -1,14 +1,1 @@
-// // const StyleDictionary = require('style-dictionary').extend('config.json');
-// import StyleDictionary from 'style-dictionary';
-// import config from './config.json';
-
-// const styleDictionary = new StyleDictionary(config);
-
-// styleDictionary.buildAllPlatforms();
-
-async () => {
-	const StyleDictionary = (await import('style-dictionary')).default;
-	const config = await import('./config.json');
-	const styleDictionary = new StyleDictionary(config);
-	styleDictionary.buildAllPlatforms();
-};
+const StyleDictionary = require('style-dictionary').extend('config.json');
