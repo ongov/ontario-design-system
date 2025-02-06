0045d0026727b70ac93b421173cb532b724f8527
Matt St Jean <matt.st.jean@ontario.ca>
2025-02-06 09:44:02 -0500

    fix(code-highlighter): add type assertion for Prism component due to incomplete types

--- packages/app-react/src/components/code-highlighter.tsx
+++ packages/app-react/src/components/code-highlighter.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { Prism, SyntaxHighlighterProps } from 'react-syntax-highlighter';
+// Type assertion needed due to incomplete types in react-syntax-highlighter
+// See: https://github.com/react-syntax-highlighter/react-syntax-highlighter/issues/539
 const SyntaxHighlighter = Prism as any as React.FC<SyntaxHighlighterProps>;
 
 interface CodeHighlighterProps {
