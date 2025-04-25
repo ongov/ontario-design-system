9927b26af4f05c6085d1be74183d37f1a724df5b
Meaghan Carrieres <meaghan.carrieres@ontario.ca>
2025-04-25 10:57:25 -0400

    chore: wip

--- packages/app-nextjs/src/app/page.tsx
+++ packages/app-nextjs/src/app/page.tsx
@@ -1,13 +1,15 @@
-import styles from './page.module.css';
+'use client';
 
-import { OntarioButton } from '@ongov/ontario-design-system-component-library-react';
+// import { OntarioButton } from '@ongov/ontario-design-system-component-library-react';
+import { OntarioBadge } from '@ongov/ontario-design-system-component-library-react';
 
 export default function Home() {
 	return (
-		<div className={styles.page}>
-			<main className={styles.main}>
-				<OntarioButton type="primary" label="Hello!" />
-			</main>
-		</div>
+		<main>
+			{/* 
+			// @ts-ignore */}
+			<OntarioBadge label="yo"></OntarioBadge>
+			{/* <OntarioButton type="primary" label="Hello!" /> */}
+		</main>
 	);
 }
