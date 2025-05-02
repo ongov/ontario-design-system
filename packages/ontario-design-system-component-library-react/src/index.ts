f70efa07b9d9b37c86dcfcca98d66ba41ff43c83
Meaghan Carrieres <meaghan.carrieres@ontario.ca>
2025-05-02 14:49:59 -0400

    build: fix react-component-library build errors
    - added type "module" to component-library and react-component-library package.json files
    - added tsx package to fix build errors within component library for build-script
    - react output target in stencil.config.ts file was updated to match current definition
    - updated dist-custom-elements to generate components folder in root of component-library
    - updated gitignore to ignore generated files from new scripts (hydrate, components)
    - cleaned up output targets of stencil.config.ts file
    
    BREAKING CHANGE: `component` folder in the component-library moved from `dist`
    to the root of the package

--- packages/ontario-design-system-component-library-react/src/index.ts
+++ packages/ontario-design-system-component-library-react/src/index.ts
@@ -1,2 +1,2 @@
 export * from './components/components.js';
-export { setAssetPath } from '@ongov/ontario-design-system-component-library/dist/components/index.js';
+export { setAssetPath } from '@ongov/ontario-design-system-component-library/components/index.js';
