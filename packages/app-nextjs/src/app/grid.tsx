0ea1dee472148aa62c88840169de954385dc0084
Meaghan Carrieres <meaghan.carrieres@ontario.ca>
2025-07-02 11:16:32 -0400

    chore: fixes typo in grid class

--- packages/app-nextjs/src/app/grid.tsx
+++ packages/app-nextjs/src/app/grid.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Grid = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="ontario-row">
-			<div className="ontario-columms ontario-small-12">{children}</div>
+			<div className="ontario-columns ontario-small-12">{children}</div>
 		</div>
 	);
 };
