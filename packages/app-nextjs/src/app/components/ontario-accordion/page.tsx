a095fc73703dbaf566c365acf3dda211dad276d1
Erin Carrieres <erin.carrieres@ontario.ca>
2025-07-03 10:13:47 -0400

    feat: create poc test page for ontario-accordion

--- packages/app-nextjs/src/app/components/ontario-accordion/page.tsx
+++ packages/app-nextjs/src/app/components/ontario-accordion/page.tsx
@@ -1,10 +1,54 @@
 import { Grid } from '../../grid';
+import { OntarioAccordion } from '@ongov/ontario-design-system-component-library-react';
 
-export default function OntarioAccordion() {
+export default function OntarioAccordionPage() {
 	return (
 		<main>
 			<Grid>
 				<h1>ontario-accordion</h1>
+
+				<h2>"accordion" Prop Variants</h2>
+
+				<h3>Name</h3>
+				<OntarioAccordion
+					name="My Accordion"
+					expand-collapse-button='{
+							"expandAllSectionsLabel": "Expand All",
+							"collapseAllSectionsLabel": "Collapse All"
+						}'
+					accordion-data='[
+							{"label": "Accordion 1", "content": ["Item 1", "Item 2", "Item 3"]},
+							{"label": "Accordion 2", "content": ["Item A", "Item B", "Item C"]}
+						]'
+				></OntarioAccordion>
+
+				<h3>Open Accordion</h3>
+				<OntarioAccordion
+					name="My Accordion"
+					isOpen={true}
+					expand-collapse-button='{
+							"expandAllSectionsLabel": "Expand All",
+							"collapseAllSectionsLabel": "Collapse All"
+						}'
+					accordion-data='[
+							{"label": "Accordion 1", "content": ["Item 1", "Item 2", "Item 3"]},
+							{"label": "Accordion 2", "content": ["Item A", "Item B", "Item C"]}
+						]'
+				></OntarioAccordion>
+
+				<h3>Closed Accordion</h3>
+				<OntarioAccordion
+					name="My Accordion"
+					isOpen={false}
+					expand-collapse-button='{
+							"expandAllSectionsLabel": "Expand All",
+							"collapseAllSectionsLabel": "Collapse All"
+						}'
+					accordion-data='[
+							{"label": "Accordion 1", "content": ["Item 1", "Item 2", "Item 3"]},
+							{"label": "Accordion 2", "content": ["Item A", "Item B", "Item C"]}
+						]'
+				></OntarioAccordion>
 			</Grid>
 		</main>
 	);
