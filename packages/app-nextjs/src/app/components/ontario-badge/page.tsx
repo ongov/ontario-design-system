cff01a34882aa22b9aff3e383bfabbe974d60082
Erin Carrieres <erin.carrieres@ontario.ca>
2025-07-07 15:04:09 -0400

    feat: added testing page for badge

--- packages/app-nextjs/src/app/components/ontario-badge/page.tsx
+++ packages/app-nextjs/src/app/components/ontario-badge/page.tsx
@@ -7,14 +7,19 @@ export default function OntarioBadgePage() {
 			<Grid>
 				<h1>ontario-badge</h1>
 
-				<h2>"Colour" Prop Variants</h2>
-
-				<h3>teal</h3>
+				<h2>"colour" Prop Variants</h2>
+				<h3>teal colour</h3>
 				<OntarioBadge colour="teal" label="Teal Badge" />
+
+				<h3>green colour</h3>
+				<OntarioBadge colour="green" label="Green Badge" />
 
 				<h2>"label" Prop Variants</h2>
 				<h3>success label</h3>
 				<OntarioBadge colour="green" label="Success" />
+
+				<h3>error label</h3>
+				<OntarioBadge colour="red" label="Error" />
 			</Grid>
 		</main>
 	);
