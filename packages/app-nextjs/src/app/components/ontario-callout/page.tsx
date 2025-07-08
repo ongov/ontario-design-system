4edd8f9d8d1759b1ff3140972ef9d5d386555208
Erin Carrieres <erin.carrieres@ontario.ca>
2025-07-08 14:42:34 -0400

    feat: added more tests for highligh colour and heading type

--- packages/app-nextjs/src/app/components/ontario-callout/page.tsx
+++ packages/app-nextjs/src/app/components/ontario-callout/page.tsx
@@ -6,25 +6,50 @@ export default function OntarioCalloutPage() {
 		<main>
 			<Grid>
 				<h1>ontario-callout</h1>
+
 				<h2>"heading-type" Prop Variants</h2>
-				<h3>h2 heading-type</h3>
+				<h3>heading type - h2</h3>
 				<OntarioCallout
 					headingType="h2"
-					headingContentType="html"
+					headingContentType="string"
 					headingContent="<a href='#'>h2 heading-type</a>"
 					content="This is testing the h2 heading-type."
 				></OntarioCallout>
 
-				<h3>h3 heading-type</h3>
+				<h3>heading type - h3</h3>
 				<OntarioCallout
 					headingType="h3"
-					headingContentType="html"
+					headingContentType="string"
 					headingContent="<a href='#'>h3 heading-type</a>"
 					content="This is testing the h3 heading-type."
 				></OntarioCallout>
 
+				<h3>heading type - h4</h3>
+				<OntarioCallout
+					headingType="h4"
+					headingContentType="string"
+					headingContent="<a href='#'>h4 heading-type</a>"
+					content="This is testing the h4 heading-type."
+				></OntarioCallout>
+
+				<h3>heading type - h5</h3>
+				<OntarioCallout
+					headingType="h5"
+					headingContentType="string"
+					headingContent="<a href='#'>h5 heading-type</a>"
+					content="This is testing the h5 heading-type."
+				></OntarioCallout>
+
+				<h3>heading type - h6</h3>
+				<OntarioCallout
+					headingType="h6"
+					headingContentType="string"
+					headingContent="<a href='#'>h6 heading-type</a>"
+					content="This is testing the h6 heading-type."
+				></OntarioCallout>
+
 				<h2>"heading-content-type" Prop Variants</h2>
-				<h3>h2 heading-content-type is html </h3>
+				<h3> heading content type - html </h3>
 				<OntarioCallout
 					headingType="h2"
 					headingContentType="html"
@@ -32,7 +57,7 @@ export default function OntarioCalloutPage() {
 					content="<div><p>This is testing the heading-content-type with HTML content.</p></div> <div><p>This is testing the h2 heading-content-type with HTML content.</p></div>"
 				></OntarioCallout>
 
-				<h3>h3 heading-content-type is string</h3>
+				<h3>heading content type - string</h3>
 				<OntarioCallout
 					headingType="h3"
 					headingContentType="string"
@@ -41,20 +66,94 @@ export default function OntarioCalloutPage() {
 				></OntarioCallout>
 
 				<h2>"highlight-colour" Prop Variants</h2>
-				<h3>yellow highlight-colour </h3>
+				<h3>highlight colour - yellow </h3>
 				<OntarioCallout
 					headingType="h2"
+					highlightColour="yellow"
 					headingContentType="string"
 					headingContent="<a href='#'>Yellow highlight colour</a>"
 					content="This is testing the yellow highlight colour."
 				></OntarioCallout>
 
-				<h3>blue highlight-colour </h3>
+				<h3>highlight colour - blue </h3>
 				<OntarioCallout
 					headingType="h2"
+					highlightColour="blue"
 					headingContentType="string"
 					headingContent="<a href='#'>Blue highlight colour</a>"
 					content="This is testing the blue highlight colour."
+				></OntarioCallout>
+
+				<h3>highlight colour - teal </h3>
+				<OntarioCallout
+					headingType="h2"
+					highlightColour="teal"
+					headingContentType="string"
+					headingContent="<a href='#'>Teal highlight colour</a>"
+					content="This is testing the teal highlight colour."
+				></OntarioCallout>
+
+				<h3>highlight colour - taupe </h3>
+				<OntarioCallout
+					headingType="h2"
+					highlightColour="taupe"
+					headingContentType="string"
+					headingContent="<a href='#'>Taupe highlight colour</a>"
+					content="This is testing the taupe highlight colour."
+				></OntarioCallout>
+
+				<h3>highlight colour - taupe </h3>
+				<OntarioCallout
+					headingType="h2"
+					highlightColour="taupe"
+					headingContentType="string"
+					headingContent="<a href='#'>Taupe highlight colour</a>"
+					content="This is testing the taupe highlight colour."
+				></OntarioCallout>
+
+				<h3>highlight colour - green </h3>
+				<OntarioCallout
+					headingType="h2"
+					highlightColour="green"
+					headingContentType="string"
+					headingContent="<a href='#'>Green highlight colour</a>"
+					content="This is testing the green highlight colour."
+				></OntarioCallout>
+
+				<h3>highlight colour - lime </h3>
+				<OntarioCallout
+					headingType="h2"
+					highlightColour="lime"
+					headingContentType="string"
+					headingContent="<a href='#'>Lime highlight colour</a>"
+					content="This is testing the lime highlight colour."
+				></OntarioCallout>
+
+				<h3>highlight colour - sky </h3>
+				<OntarioCallout
+					headingType="h2"
+					highlightColour="sky"
+					headingContentType="string"
+					headingContent="<a href='#'>Sky highlight colour</a>"
+					content="This is testing the sky highlight colour."
+				></OntarioCallout>
+
+				<h3>highlight colour - blue </h3>
+				<OntarioCallout
+					headingType="h2"
+					highlightColour="blue"
+					headingContentType="string"
+					headingContent="<a href='#'>Blue highlight colour</a>"
+					content="This is testing the blue highlight colour."
+				></OntarioCallout>
+
+				<h3>highlight colour - purple </h3>
+				<OntarioCallout
+					headingType="h2"
+					highlightColour="purple"
+					headingContentType="string"
+					headingContent="<a href='#'>Purple highlight colour</a>"
+					content="This is testing the purple highlight colour."
 				></OntarioCallout>
 			</Grid>
 		</main>
