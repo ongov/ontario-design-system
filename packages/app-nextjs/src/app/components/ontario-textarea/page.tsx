2136659ebad23c91aba58fd226dedb84a7dabcef
Meaghan Carrieres <meaghan.carrieres@ontario.ca>
2025-07-08 09:45:14 -0400

    fix: adds html and string options for hint text and hint expander

--- packages/app-nextjs/src/app/components/ontario-textarea/page.tsx
+++ packages/app-nextjs/src/app/components/ontario-textarea/page.tsx
@@ -66,10 +66,24 @@ export default function OntarioTextareaPage() {
 				<div>
 					<h2>"hint-text" Prop Variant</h2>
 
+					<h3>string hint-content-type</h3>
 					<OntarioTextarea
 						caption={{ captionText: 'What went well during this sprint?' }}
-						name="textarea-hint-text"
-						hintText="Include team wins, individual contributions, or process improvements."
+						name="textarea-hint-text-string"
+						hintText={{
+							hint: 'Include team wins, individual contributions, or process improvements.',
+							hintContentType: 'string',
+						}}
+					></OntarioTextarea>
+
+					<h3>html hint-content-type</h3>
+					<OntarioTextarea
+						caption={{ captionText: 'What can the team improve?' }}
+						name="textarea-hint-text-html"
+						hintText={{
+							hint: '<p>Your feedback helps us grow.</p><p>What could we tweak, fix, or rethink to improve how we work?</p>',
+							hintContentType: 'html',
+						}}
 					></OntarioTextarea>
 				</div>
 
@@ -78,13 +92,27 @@ export default function OntarioTextareaPage() {
 				<div>
 					<h2>"hint-expander" Prop Variant</h2>
 
+					<h3>string hint-content-type</h3>
 					<OntarioTextarea
 						caption={{ captionText: 'Tell us about your ideal vacation' }}
-						name="textarea-hint-expander"
+						name="textarea-hint-expander-string"
 						hintExpander={{
 							hint: 'Not sure what to write?',
 							content:
 								'Please describe your ideal vacation in detail, including destination, activities, and duration. This will help us understand your preferences better.',
+							hintContentType: 'string',
+						}}
+					></OntarioTextarea>
+
+					<h3>html hint content type</h3>
+					<OntarioTextarea
+						caption={{ captionText: 'What can the team improve?' }}
+						name="textarea-hint-expander-html"
+						hintExpander={{
+							hint: 'What is meant by "improve"?',
+							content:
+								'<p>This question is about anything you think could work better — big or small.</p><ul><li>Was something confusing or inefficient?</li><li>Did communication feel unclear or delayed?</li><li>Were tools, timelines, or roles a bit off?</li></ul><p>You can focus on processes, collaboration, tools, meetings — anything you think could make our work smoother or more effective.</p>',
+							hintContentType: 'html',
 						}}
 					></OntarioTextarea>
 				</div>
