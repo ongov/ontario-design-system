6842e68391dce1262b0405cca63ad71ed2b605a2
Erin Carrieres <erin.carrieres@ontario.ca>
2025-07-30 10:46:55 -0400

    test: fixed spec test to match new html layout

--- packages/ontario-design-system-component-library/src/components/ontario-task/test/ontario-task.spec.tsx
+++ packages/ontario-design-system-component-library/src/components/ontario-task/test/ontario-task.spec.tsx
@@ -18,8 +18,10 @@ describe('ontario-task', () => {
 								<li aria-labelledby="task-label--task-1" class="ontario-task ontario-task-status--not-started" data-task-status="notStarted" role="group">
 											<div>
 													<div class="ontario-task__content">
+														<div class="ontario-task__text">
 															<h3 class="ontario-task__label" id="task-label--task-1"></h3>
-															<ontario-badge aria-label="Task Status: Not Started (notStarted)" class="ontario-task__badge" colour="lightTeal" role="status">
+														</div>
+														<ontario-badge aria-label="Task Status: Not Started (notStarted)" class="ontario-task__badge" colour="lightTeal" role="status">
 																	Not Started
 															</ontario-badge>
 													</div>
