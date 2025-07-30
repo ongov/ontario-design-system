ea422d9bddcb102bbcb2c2147a2a9aaead399df8
Erin Carrieres <erin.carrieres@ontario.ca>
2025-07-30 13:14:55 -0400

    test: added consistency to ontario-badge testing

--- packages/app-nextjs/src/app/components/ontario-badge/page.tsx
+++ packages/app-nextjs/src/app/components/ontario-badge/page.tsx
@@ -8,35 +8,35 @@ export default function OntarioBadgePage() {
 				<h1>ontario-badge</h1>
 
 				<h2>"colour" Prop Variants</h2>
-				<h3>teal colour</h3>
+				<h3>teal</h3>
 				<OntarioBadge colour="teal" label="Teal Badge" />
 
-				<h3>green colour</h3>
+				<h3>green</h3>
 				<OntarioBadge colour="green" label="Green Badge" />
 
-				<h3>light-teal colour</h3>
+				<h3>light-teal</h3>
 				<OntarioBadge colour="lightTeal" label="Light Teal Badge" />
 
-				<h3>black colour</h3>
+				<h3>black</h3>
 				<OntarioBadge colour="black" label="Black Badge" />
 
-				<h3>red colour</h3>
+				<h3>red</h3>
 				<OntarioBadge colour="red" label="Red Badge" />
 
-				<h3>green colour</h3>
+				<h3>green</h3>
 				<OntarioBadge colour="green" label="Green Badge" />
 
-				<h3>white colour</h3>
+				<h3>white</h3>
 				<OntarioBadge colour="white" label="White Badge" />
 
-				<h3>dark grey colour</h3>
+				<h3>dark grey</h3>
 				<OntarioBadge colour="darkGrey" label="Dark Grey Badge" />
 
 				<h2>"label" Prop Variants</h2>
-				<h3>success label</h3>
+				<h3>success</h3>
 				<OntarioBadge colour="green" label="Success" />
 
-				<h3>error label</h3>
+				<h3>error</h3>
 				<OntarioBadge colour="red" label="Error" />
 			</Grid>
 		</main>
