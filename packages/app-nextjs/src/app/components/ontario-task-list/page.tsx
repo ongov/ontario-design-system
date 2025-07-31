cfab98d159b03e69fb1ea3b79eb6787150d56757
Erin Carrieres <erin.carrieres@ontario.ca>
2025-07-31 16:37:28 -0400

    test: updated headings to keep consistency

--- packages/app-nextjs/src/app/components/ontario-task-list/page.tsx
+++ packages/app-nextjs/src/app/components/ontario-task-list/page.tsx
@@ -1,3 +1,4 @@
+import { TaskStatuses } from '@ongov/ontario-design-system-component-library';
 import { Grid } from '../../grid';
 import { OntarioTaskList, OntarioTask } from '@ongov/ontario-design-system-component-library-react';
 
@@ -8,37 +9,37 @@ export default function OntarioTaskListPage() {
 				<h1>OntarioTaskList</h1>
 
 				<h2>'language' Prop Variants</h2>
-				<h3>Language - en</h3>
+				<h3>EN</h3>
 				<OntarioTaskList label="English Task List" language="en">
-					<OntarioTask taskId="task-1" label="Task 1" taskStatus="Completed" />
-					<OntarioTask taskId="task-2" label="Task 2" taskStatus="NotStarted" />
+					<OntarioTask taskId="task-1" label="Task 1" taskStatus={TaskStatuses.Completed} />
+					<OntarioTask taskId="task-2" label="Task 2" taskStatus={TaskStatuses.NotStarted} />
 				</OntarioTaskList>
 
-				<h3>Language - fr</h3>
+				<h3>FR</h3>
 				<OntarioTaskList label="Liste de tâches en français" language="fr">
-					<OntarioTask taskId="task-3" label="Tâche 1" taskStatus="Completed" />
-					<OntarioTask taskId="task-4" label="Tâche 2" taskStatus="InProgress" />
+					<OntarioTask taskId="task-3" label="Tâche 1" taskStatus={TaskStatuses.Completed} />
+					<OntarioTask taskId="task-4" label="Tâche 2" taskStatus={TaskStatuses.InProgress} />
 				</OntarioTaskList>
 
 				<h2>'heading-level' Prop Variants</h2>
-				<h3>heading level - h1</h3>
+				<h3>H1</h3>
 				<OntarioTaskList label="Heading Level H1" headingLevel="h1">
-					<OntarioTask taskId="task-5" label="Task A" taskStatus="NotStarted" />
+					<OntarioTask taskId="task-5" label="Task A" taskStatus={TaskStatuses.NotStarted} />
 				</OntarioTaskList>
 
-				<h3>heading level - h2</h3>
+				<h3>H2</h3>
 				<OntarioTaskList label="Heading Level H2" headingLevel="h2">
-					<OntarioTask taskId="task-6" label="Task B" taskStatus="InProgress" />
+					<OntarioTask taskId="task-6" label="Task B" taskStatus={TaskStatuses.InProgress} />
 				</OntarioTaskList>
 
-				<h3>heading level - h3</h3>
+				<h3>H3</h3>
 				<OntarioTaskList label="Heading Level H3" headingLevel="h3">
-					<OntarioTask taskId="task-7" label="Task C" taskStatus="Completed" />
+					<OntarioTask taskId="task-7" label="Task C" taskStatus={TaskStatuses.Completed} />
 				</OntarioTaskList>
 
-				<h3>heading level - h4</h3>
+				<h3>H4</h3>
 				<OntarioTaskList label="Heading Level H4" headingLevel="h4">
-					<OntarioTask taskId="task-8" label="Task D" taskStatus="NotStarted" />
+					<OntarioTask taskId="task-8" label="Task D" taskStatus={TaskStatuses.NotStarted} />
 				</OntarioTaskList>
 			</Grid>
 		</main>
