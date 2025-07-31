c88c3d8850a53faf7bc33f0fd993bab3fb58366f
Erin Carrieres <erin.carrieres@ontario.ca>
2025-07-31 16:01:25 -0400

    test: updated headings to keep consistency

--- packages/app-nextjs/src/app/components/ontario-critical-alert/page.tsx
+++ packages/app-nextjs/src/app/components/ontario-critical-alert/page.tsx
@@ -5,14 +5,14 @@ export default function OntarioCriticalAlertPage() {
 	return (
 		<main>
 			<Grid>
-				<h1>OntarioCriticalAlert</h1>
+				<h1>ontario-critical-alert</h1>
 
 				<h2>'content' Prop Variants</h2>
 
-				<h3>Content - string</h3>
+				<h3>string</h3>
 				<OntarioCriticalAlert content="COVID-19 State of emergency extended until May 12, 2020." />
 
-				<h3>Content - slot (HTML)</h3>
+				<h3>slot (HTML)</h3>
 				<OntarioCriticalAlert>
 					<a href="#">COVID-19 State of emergency</a> extended until May 12, 2020.
 				</OntarioCriticalAlert>
