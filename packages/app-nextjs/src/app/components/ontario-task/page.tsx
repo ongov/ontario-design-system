73509bc4dc40ca91f1e9b9e052b610bf77f38c83
Erin Carrieres <erin.carrieres@ontario.ca>
2025-07-31 16:37:28 -0400

    test: updated headings to keep consistency

--- packages/app-nextjs/src/app/components/ontario-task/page.tsx
+++ packages/app-nextjs/src/app/components/ontario-task/page.tsx
@@ -1,3 +1,4 @@
+import { TaskStatuses } from '@ongov/ontario-design-system-component-library';
 import { Grid } from '../../grid';
 import { OntarioTask } from '@ongov/ontario-design-system-component-library-react';
 
@@ -8,34 +9,34 @@ export default function OntarioTaskPage() {
 				<h1>OntarioTask</h1>
 
 				<h2>'language' Prop Variants</h2>
-				<h3>Language - en</h3>
+				<h3>EN</h3>
 				<OntarioTask taskId="task-en" label="English Task" language="en" />
 
-				<h3>Language - fr</h3>
+				<h3>FR</h3>
 				<OntarioTask taskId="task-fr" label="Tâche en français" language="fr" />
 
 				<h2>'task-status' Prop Variants</h2>
-				<h2>taskStatus - NotStarted</h2>
-				<OntarioTask taskId="task-notstarted" label="Task Not Started" taskStatus="NotStarted" />
+				<h3>Not started</h3>
+				<OntarioTask taskId="task-notstarted" label="Task Not Started" taskStatus={TaskStatuses.NotStarted} />
 
-				<h2>taskStatus - InProgress</h2>
-				<OntarioTask taskId="task-inprogress" label="Task In Progress" taskStatus="InProgress" />
+				<h3>In progress</h3>
+				<OntarioTask taskId="task-inprogress" label="Task In Progress" taskStatus={TaskStatuses.InProgress} />
 
-				<h2>taskStatus - Completed</h2>
-				<OntarioTask taskId="task-completed" label="Task Completed" taskStatus="Completed" />
+				<h3>Completed</h3>
+				<OntarioTask taskId="task-completed" label="Task Completed" taskStatus={TaskStatuses.Completed} />
 
 				<h2>'headingLevel' - Prop Variants</h2>
-				<h3>heading level - h2</h3>
+				<h3>H2</h3>
 				<OntarioTask taskId="task-h2" label="Heading Level H2" headingLevel="h2" />
 
-				<h3>heading level - h3</h3>
+				<h3>H3</h3>
 				<OntarioTask taskId="task-h3" label="Heading Level H3" headingLevel="h3" />
 
-				<h3>heading level - h4</h3>
+				<h3>H4</h3>
 				<OntarioTask taskId="task-h4" label="Heading Level H4" headingLevel="h4" />
 
 				<h2>'deavtivate-link' - Prop Variants</h2>
-				<h3>deactivate link - true</h3>
+				<h3>True</h3>
 				<OntarioTask
 					taskId="task-deactivated"
 					label="Deactivated Link Task"
@@ -43,7 +44,7 @@ export default function OntarioTaskPage() {
 					deactivateLink={true}
 				/>
 
-				<h3>deactivate link - false</h3>
+				<h3>False</h3>
 				<OntarioTask taskId="task-active" label="Active Link Task" link="https://example.com" deactivateLink={false} />
 
 				<h2>hintText - string</h2>
