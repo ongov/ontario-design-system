aad52bf2a4538d4a2ccaa5d410177059337efa42
Meaghan Carrieres <meaghan.carrieres@ontario.ca>
2025-08-25 13:56:40 -0400

    chore: adds ontario-fieldset page to app-nextjs PoC
    - adds ontario-fieldset page + component to app-nextjs PoC with all variants
    - updates order of pages in main page.tsx file of app-nextjs PoC
    - fixes bugs in ontario-accordion, ontario-task and ontario-task-list pages

--- packages/app-nextjs/src/app/components/ontario-accordion/page.tsx
+++ packages/app-nextjs/src/app/components/ontario-accordion/page.tsx
@@ -13,26 +13,29 @@ export default function OntarioAccordionPage() {
 					expandCollapseButton={{
 						expandAllSectionsLabel: 'Expand All',
 						collapseAllSectionsLabel: 'Collapse All',
+						ariaLabelText: 'Expand or collapse all accordion sections',
 					}}
 					accordionData={[
 						{
 							label: 'Accordion 1',
-							content: [
-								'Ontario is Canada’s most populous province.',
-								'The capital city is Toronto.',
-								'Lake Superior is the largest of the Great Lakes.',
-							],
+							accordionContentType: 'html',
+							ariaLabelText: 'List of Ontario, Toronto and Lake Superior facts',
+							content:
+								'<ul><li>Ontario is Canada’s most populous province.</li><li>The capital city is Toronto.</li><li>Lake Superior is the largest of the Great Lakes.</li></ul>',
+							isOpen: false,
 						},
 						{
 							label: 'Accordion 2',
-							content: [
-								'Bananas are berries, but strawberries are not.',
-								'The Eiffel Tower can be 15 cm taller during hot days.',
-								'Honey never spoils.',
-							],
+							accordionContentType: 'html',
+							ariaLabelText: 'List of banana, Eiffel Tower and honey facts',
+							isOpen: false,
+							content:
+								'<ul><li>Bananas are berries, but strawberries aren’t.</li><li>The Eiffel Tower can be 15 cm taller during the summer.</li><li>Honey never spoils.</li></ul>',
 						},
 					]}
 				/>
+
+				<hr />
 
 				<h2>"is-open" Prop Variant</h2>
 
@@ -43,26 +46,29 @@ export default function OntarioAccordionPage() {
 					expandCollapseButton={{
 						expandAllSectionsLabel: 'Expand All',
 						collapseAllSectionsLabel: 'Collapse All',
+						ariaLabelText: 'Expand or collapse all accordion sections',
 					}}
 					accordionData={[
 						{
 							label: 'Accordion 1',
-							content: [
-								'Cats have fewer toes on their back paws.',
-								'The unicorn is Scotland’s national animal.',
-								'Octopuses have three hearts.',
-							],
+							accordionContentType: 'html',
+							content:
+								'<ul><li>Cats have fewer toes on their back paws.</li><li>The unicorn is Scotland’s national animal.</li><li>Octopuses have three hearts.</li></ul>',
+							ariaLabelText: 'List of cat, unicorn and octopus facts',
+							isOpen: true,
 						},
 						{
 							label: 'Accordion 2',
-							content: [
-								'The world’s largest desert is Antarctica.',
-								'Wombat poop is cube-shaped.',
-								'There are more stars in the universe than grains of sand on Earth.',
-							],
+							accordionContentType: 'html',
+							ariaLabelText: 'List of desert, wombat and star facts',
+							isOpen: true,
+							content:
+								'<ul><li>The world’s largest desert is Antarctica.</li><li>Wombat poop is cube-shaped.</li><li>There are more stars in the universe than grains of sand on Earth.</li></ul>',
 						},
 					]}
 				/>
+
+				<hr />
 
 				<h3>Closed Accordion</h3>
 				<OntarioAccordion
@@ -71,26 +77,29 @@ export default function OntarioAccordionPage() {
 					expandCollapseButton={{
 						expandAllSectionsLabel: 'Expand All',
 						collapseAllSectionsLabel: 'Collapse All',
+						ariaLabelText: 'Expand or collapse all accordion sections',
 					}}
 					accordionData={[
 						{
 							label: 'Accordion 1',
-							content: [
-								'Hot water freezes faster than cold water.',
-								'The inventor of the frisbee was turned into a frisbee.',
-								'The dot over the letter "i" is called a tittle.',
-							],
+							accordionContentType: 'string',
+							content:
+								'We choose to go to the moon. We choose to go to the moon in this decade and do the other things, not because they are easy, but because they are hard, because that goal will serve to organize and measure the best of our energies and skills, because that challenge is one that we are willing to accept, one we are unwilling to postpone, and one which we intend to win, and the others, too.',
+							ariaLabelText: 'Famous quote from John F. Kennedy about going to the moon',
+							isOpen: false,
 						},
 						{
 							label: 'Accordion 2',
-							content: [
-								'Venus is the only planet to spin clockwise.',
-								'The tongue is the only muscle attached at one end.',
-								'Some turtles can breathe through their butts.',
-							],
+							accordionContentType: 'string',
+							content:
+								'Two roads diverged in a wood, and I— I took the one less traveled by, And that has made all the difference.',
+							ariaLabelText: "Famous quote from Robert Frost's The Road Not Taken",
+							isOpen: false,
 						},
 					]}
 				/>
+
+				<hr />
 
 				<h2>"expand-collapse-button" Prop Variant</h2>
 				<OntarioAccordion
@@ -99,23 +108,24 @@ export default function OntarioAccordionPage() {
 					expandCollapseButton={{
 						expandAllSectionsLabel: 'Open all accordions',
 						collapseAllSectionsLabel: 'Close all accordions',
+						ariaLabelText: 'Expand or collapse all accordion sections',
 					}}
 					accordionData={[
 						{
 							label: 'Accordion 1',
-							content: [
-								'The longest place name on the planet is 85 letters long.',
-								'The inventor of the microwave only received $2 for his discovery.',
-								'The first computer mouse was made of wood.',
-							],
+							accordionContentType: 'string',
+							content:
+								'I believe that space travel will one day become as common as airline travel is today. I’m convinced, however, that the true future of space travel does not lie with government agencies — NASA is still obsessed with the idea that the primary purpose of the space program is science — but real progress will come from private companies competing to provide the ultimate adventure ride, and NASA will receive the trickle-down benefits.',
+							ariaLabelText: 'Famous quote from Buzz Aldrin - Magnificent Desolation',
+							isOpen: false,
 						},
 						{
 							label: 'Accordion 2',
-							content: [
-								'The Twitter bird’s official name is Larry.',
-								'The majority of your brain is fat.',
-								'The hashtag symbol is technically called an octothorpe.',
-							],
+							accordionContentType: 'string',
+							content:
+								'Across the sea of space, the stars are other suns. We are made of star-stuff. We are a way for the cosmos to know itself. Some part of our being knows this is where we came from. We long to return. And we can, because the cosmos is also within us. We’re made of star-stuff. We are a way for the universe to know itself.',
+							ariaLabelText: 'Famous quote from Carl Sagan - Cosmos',
+							isOpen: false,
 						},
 					]}
 				/>
