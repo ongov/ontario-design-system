f8399b82e3ddb3ef7224a3ed7d74443fb12d73c2
Meaghan Carrieres <meaghan.carrieres@ontario.ca>
2025-08-25 13:56:40 -0400

    chore: adds ontario-fieldset page to app-nextjs PoC
    - adds ontario-fieldset page + component to app-nextjs PoC with all variants
    - updates order of pages in main page.tsx file of app-nextjs PoC
    - fixes bugs in ontario-accordion, ontario-task and ontario-task-list pages

--- packages/app-nextjs/src/app/page.tsx
+++ packages/app-nextjs/src/app/page.tsx
@@ -21,37 +21,16 @@ export default function Home() {
 							<Link href="/components/ontario-accordion">ontario-accordion</Link>
 						</li>
 						<li>
+							<Link href="/components/ontario-aside">ontario-aside</Link>
+						</li>
+						<li>
+							<Link href="/components/ontario-back-to-top">ontario-back-to-top</Link>
+						</li>
+						<li>
 							<Link href="/components/ontario-badge">ontario-badge</Link>
 						</li>
 						<li>
 							<Link href="/components/ontario-button">ontario-button</Link>
-						</li>
-						<li>
-							<Link href="/components/ontario-card-collection">ontario-card-collection</Link>
-						</li>
-						<li>
-							<Link href="/components/ontario-critical-alert">ontario-critical-alert</Link>
-						</li>
-						<li>
-							<Link href="/components/ontario-hint-text">ontario-hint-text</Link>
-						</li>
-						<li>
-							<Link href="/components/ontario-input">ontario-input</Link>
-						</li>
-						<li>
-							<Link href="/components/ontario-language-toggle">ontario-language-toggle</Link>
-						</li>
-						<li>
-							<Link href="/components/ontario-textarea">ontario-textarea</Link>
-						</li>
-						<li>
-							<Link href="/components/ontario-page-alert">ontario-page-alert</Link>
-						</li>
-						<li>
-							<Link href="/components/ontario-hint-expander">ontario-hint-expander</Link>
-						</li>
-						<li>
-							<Link href="/components/ontario-aside">ontario-aside</Link>
 						</li>
 						<li>
 							<Link href="/components/ontario-callout">ontario-callout</Link>
@@ -60,25 +39,50 @@ export default function Home() {
 							<Link href="/components/ontario-card">ontario-card</Link>
 						</li>
 						<li>
-							<Link href="/components/ontario-search-box">ontario-search-box</Link>
+							<Link href="/components/ontario-card-collection">ontario-card-collection</Link>
+						</li>
+						<li>
+							<Link href="/components/ontario-critical-alert">ontario-critical-alert</Link>
 						</li>
 						<li>
 							<Link href="/components/ontario-date-input">ontario-date-input</Link>
 						</li>
 						<li>
-							<Link href="/components/ontario-dropdown-list">ontario-search-box</Link>
+							<Link href="/components/ontario-dropdown-list">ontario-dropdown-list</Link>
 						</li>
 						<li>
-							<Link href="/components/ontario-back-to-top">ontario-back-to-top</Link>
+							<Link href="/components/ontario-fieldset">ontario-fieldset</Link>
+						</li>
+						<li>
+							<Link href="/components/ontario-hint-expander">ontario-hint-expander</Link>
+						</li>
+						<li>
+							<Link href="/components/ontario-hint-text">ontario-hint-text</Link>
 						</li>
 						<li>
 							<Link href="/components/ontario-icon">ontario-icon</Link>
+						</li>
+						<li>
+							<Link href="/components/ontario-input">ontario-input</Link>
+						</li>
+						<li>
+							<Link href="/components/ontario-language-toggle">ontario-language-toggle</Link>
+						</li>
+
+						<li>
+							<Link href="/components/ontario-page-alert">ontario-page-alert</Link>
+						</li>
+						<li>
+							<Link href="/components/ontario-search-box">ontario-search-box</Link>
 						</li>
 						<li>
 							<Link href="/components/ontario-task">ontario-task</Link>
 						</li>
 						<li>
 							<Link href="/components/ontario-task-list">ontario-task-list</Link>
+						</li>
+						<li>
+							<Link href="/components/ontario-textarea">ontario-textarea</Link>
 						</li>
 					</ul>
 				</div>
