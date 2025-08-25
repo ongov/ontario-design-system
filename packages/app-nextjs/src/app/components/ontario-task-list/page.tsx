7f71733dfd467650a157be0c5a8fdf9f7a2141bf
Meaghan Carrieres <meaghan.carrieres@ontario.ca>
2025-08-25 13:56:40 -0400

    chore: adds ontario-fieldset page to app-nextjs PoC
    - adds ontario-fieldset page + component to app-nextjs PoC with all variants
    - updates order of pages in main page.tsx file of app-nextjs PoC
    - fixes bugs in ontario-accordion, ontario-task and ontario-task-list pages

--- packages/app-nextjs/src/app/components/ontario-task-list/page.tsx
+++ packages/app-nextjs/src/app/components/ontario-task-list/page.tsx
@@ -1,4 +1,3 @@
-import { TaskStatuses } from '@ongov/ontario-design-system-component-library';
 import { Grid } from '../../grid';
 import { OntarioTaskList, OntarioTask } from '@ongov/ontario-design-system-component-library-react';
 
@@ -11,35 +10,35 @@ export default function OntarioTaskListPage() {
 				<h2>'language' Prop Variants</h2>
 				<h3>EN</h3>
 				<OntarioTaskList label="English Task List" language="en">
-					<OntarioTask taskId="task-1" label="Task 1" taskStatus={TaskStatuses.Completed} />
-					<OntarioTask taskId="task-2" label="Task 2" taskStatus={TaskStatuses.NotStarted} />
+					<OntarioTask taskId="task-1" label="Task 1" task-status="completed" />
+					<OntarioTask taskId="task-2" label="Task 2" task-status="notStarted" />
 				</OntarioTaskList>
 
 				<h3>FR</h3>
 				<OntarioTaskList label="Liste de tâches en français" language="fr">
-					<OntarioTask taskId="task-3" label="Tâche 1" taskStatus={TaskStatuses.Completed} />
-					<OntarioTask taskId="task-4" label="Tâche 2" taskStatus={TaskStatuses.InProgress} />
+					<OntarioTask taskId="task-3" label="Tâche 1" task-status="completed" />
+					<OntarioTask taskId="task-4" label="Tâche 2" task-status="inProgress" />
 				</OntarioTaskList>
 
 				<h2>'heading-level' Prop Variants</h2>
 				<h3>H1</h3>
 				<OntarioTaskList label="Heading Level H1" headingLevel="h1">
-					<OntarioTask taskId="task-5" label="Task A" taskStatus={TaskStatuses.NotStarted} />
+					<OntarioTask taskId="task-5" label="Task A" task-status="notStarted" />
 				</OntarioTaskList>
 
 				<h3>H2</h3>
 				<OntarioTaskList label="Heading Level H2" headingLevel="h2">
-					<OntarioTask taskId="task-6" label="Task B" taskStatus={TaskStatuses.InProgress} />
+					<OntarioTask taskId="task-6" label="Task B" task-status="inProgress" />
 				</OntarioTaskList>
 
 				<h3>H3</h3>
 				<OntarioTaskList label="Heading Level H3" headingLevel="h3">
-					<OntarioTask taskId="task-7" label="Task C" taskStatus={TaskStatuses.Completed} />
+					<OntarioTask taskId="task-7" label="Task C" task-status="completed" />
 				</OntarioTaskList>
 
 				<h3>H4</h3>
 				<OntarioTaskList label="Heading Level H4" headingLevel="h4">
-					<OntarioTask taskId="task-8" label="Task D" taskStatus={TaskStatuses.NotStarted} />
+					<OntarioTask taskId="task-8" label="Task D" task-status="notStarted" />
 				</OntarioTaskList>
 			</Grid>
 		</main>
