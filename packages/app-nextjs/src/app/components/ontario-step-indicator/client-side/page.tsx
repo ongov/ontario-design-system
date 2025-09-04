d94893e1e2a3ec4a4be7d7fa088e0e070d82b392
Erin Carrieres <erin.carrieres@ontario.ca>
2025-09-04 11:15:26 -0400

    chore: rebase

---
+++ packages/app-nextjs/src/app/components/ontario-step-indicator/client-side/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { Grid } from '../../../grid';
+import { OntarioStepIndicator } from '@ongov/ontario-design-system-component-library-react';
+
+export default function OntarioStepIndicatorClientSidePage() {
+	return (
+		<main>
+			<Grid>
+				<h1>OntarioStepIndicator (Client Side)</h1>
+				<h2>With Back Button (Custom Click)</h2>
+				<OntarioStepIndicator
+					showBackButton={true}
+					currentStep={1}
+					numberOfSteps={3}
+					customOnClick={() => alert('Back button clicked!')}
+				/>
+
+				{/* You can add more client-only tests here */}
+			</Grid>
+		</main>
+	);
+}
---
+++ packages/app-nextjs/src/app/components/ontario-step-indicator/page.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { Grid } from '../../grid';
+
+export default function OntarioStepIndicatorPage() {
+	return (
+		<main>
+			<Grid>
+				<h1>ontario-date-input</h1>
+				<div>
+					<ul>
+						<li>
+							<Link href="/components/ontario-step-indicator/client-side">Client Side Page</Link>
+						</li>
+						<li>
+							<Link href="/components/ontario-step-indicator/server-side">Server Side Page</Link>
+						</li>
+					</ul>
+				</div>
+			</Grid>
+		</main>
+	);
+}
