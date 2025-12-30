d1caec0f17193974302bfcfda10114104a70b4e7
Kyle Binger <kyle.binger3@ontario.ca>
2025-12-30 17:03:46 -0500

    chore(e2e tests): comment out old puppeteer tests

--- packages/ontario-design-system-component-library/src/components/ontario-step-indicator/test/ontario-step-indicator.e2e.ts
+++ packages/ontario-design-system-component-library/src/components/ontario-step-indicator/test/ontario-step-indicator.e2e.ts
@@ -1,25 +1,25 @@
-import { newE2EPage } from '@stencil/core/testing';
+// import { newE2EPage } from '@stencil/core/testing';
 
-describe('ontario-step-indicator', () => {
-	it('renders', async () => {
-		const page = await newE2EPage();
-		await page.setContent('<ontario-step-indicator></ontario-step-indicator>');
+// describe('ontario-step-indicator', () => {
+// 	it('renders', async () => {
+// 		const page = await newE2EPage();
+// 		await page.setContent('<ontario-step-indicator></ontario-step-indicator>');
 
-		const component = await page.find('ontario-step-indicator');
+// 		const component = await page.find('ontario-step-indicator');
 
-		expect(component).toHaveClass('hydrated');
-	});
+// 		expect(component).toHaveClass('hydrated');
+// 	});
 
-	it('renders back button with link', async () => {
-		const page = await newE2EPage();
-		await page.setContent(
-			'<ontario-step-indicator show-back-button="true" back-button-url="https://designsystem.ontario.ca/"></ontario-step-indicator>',
-		);
+// 	it('renders back button with link', async () => {
+// 		const page = await newE2EPage();
+// 		await page.setContent(
+// 			'<ontario-step-indicator show-back-button="true" back-button-url="https://designsystem.ontario.ca/"></ontario-step-indicator>',
+// 		);
 
-		const component = await page.find('ontario-step-indicator');
-		const element = await page.find('ontario-step-indicator >>> a');
+// 		const component = await page.find('ontario-step-indicator');
+// 		const element = await page.find('ontario-step-indicator >>> a');
 
-		expect(component).toHaveClass('hydrated');
-		expect(element).toHaveClasses(['ontario-button', 'ontario-button--tertiary']);
-	});
-});
+// 		expect(component).toHaveClass('hydrated');
+// 		expect(element).toHaveClasses(['ontario-button', 'ontario-button--tertiary']);
+// 	});
+// });
