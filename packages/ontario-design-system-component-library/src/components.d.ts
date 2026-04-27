99042cfa8d2d19760137c2dca43826381a7fb199
Scott Morris <scott.morris@ontario.ca>
2026-04-27 16:10:05 -0400

    chore: update lockfile and component typings

--- packages/ontario-design-system-component-library/src/components.d.ts
+++ packages/ontario-design-system-component-library/src/components.d.ts
@@ -744,6 +744,11 @@ export namespace Components {
 		 */
 		autoDetectMode?: boolean;
 		/**
+		 * Whether focus should move to the active tab when the menu opens. This should only be true for keyboard-triggered opens.
+		 * @default false
+		 */
+		focusActiveTabOnOpen: boolean;
+		/**
 		 * The language of the component. This is used for translations, and is by default set through event listeners checking for a language property from the header. If none is passed, it will default to English.
 		 * @default 'en'
 		 */
@@ -783,6 +788,11 @@ export namespace Components {
 	 * - https://designsystem.ontario.ca/developer-docs/components/ontario-header-overflow-menu/
 	 */
 	interface OntarioHeaderOverflowMenu {
+		/**
+		 * Whether the standalone menu should move focus to the first item when opened. This should only be true for keyboard-triggered opens.
+		 * @default false
+		 */
+		focusFirstItemOnOpen?: boolean;
 		/**
 		 * Whether this is the last menu in a series of menus. If true, Tab from last item goes to next element on page. If false, Tab from last item emits focusNextElement for header to handle.
 		 * @default true
@@ -5960,6 +5970,11 @@ declare namespace LocalJSX {
 		 */
 		autoDetectMode?: boolean;
 		/**
+		 * Whether focus should move to the active tab when the menu opens. This should only be true for keyboard-triggered opens.
+		 * @default false
+		 */
+		focusActiveTabOnOpen?: boolean;
+		/**
 		 * The language of the component. This is used for translations, and is by default set through event listeners checking for a language property from the header. If none is passed, it will default to English.
 		 * @default 'en'
 		 */
@@ -6011,6 +6026,11 @@ declare namespace LocalJSX {
 	 * - https://designsystem.ontario.ca/developer-docs/components/ontario-header-overflow-menu/
 	 */
 	interface OntarioHeaderOverflowMenu {
+		/**
+		 * Whether the standalone menu should move focus to the first item when opened. This should only be true for keyboard-triggered opens.
+		 * @default false
+		 */
+		focusFirstItemOnOpen?: boolean;
 		/**
 		 * Whether this is the last menu in a series of menus. If true, Tab from last item goes to next element on page. If false, Tab from last item emits focusNextElement for header to handle.
 		 * @default true
