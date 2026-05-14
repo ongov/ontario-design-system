57ce760305e35e62b0b8f8f8204533626555ffa2
Scott Morris <scott.morris@ontario.ca>
2026-05-14 17:27:49 -0400

    fix(e2e): await createConfig in playwright.config.ts for @stencil/playwright 0.4.x

--- packages/ontario-design-system-component-library/playwright.config.ts
+++ packages/ontario-design-system-component-library/playwright.config.ts
@@ -4,7 +4,7 @@ import { matchers, createConfig } from '@stencil/playwright';
 // Add custom Stencil matchers to Playwright assertions
 expect.extend(matchers);
 
-export default createConfig({
+export default await createConfig({
 	// Overwrite Playwright config options here
 	retries: process.env.CI ? 2 : 0,
 	use: {
