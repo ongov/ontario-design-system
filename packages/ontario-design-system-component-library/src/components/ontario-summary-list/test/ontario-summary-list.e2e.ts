27a8ba8b89e2ea65f46ba580bdb525c9f5d9495d
Munta Islam <munta.islam@ontario.ca>
2026-05-20 12:39:26 -0400

    test(ontario-summary-list): add component test stubs

---
+++ packages/ontario-design-system-component-library/src/components/ontario-summary-list/test/ontario-summary-list.e2e.ts
@@ -0,0 +1,11 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('ontario-summary-list', () => {
+	it('renders', async () => {
+		const page = await newE2EPage();
+		await page.setContent('<ontario-summary-list></ontario-summary-list>');
+
+		const element = await page.find('ontario-summary-list');
+		expect(element).toHaveClass('hydrated');
+	});
+});
---
+++ packages/ontario-design-system-component-library/src/components/ontario-summary-list/test/ontario-summary-list.spec.tsx
@@ -0,0 +1,18 @@
+import { newSpecPage } from '@stencil/core/testing';
+import { OntarioSummaryList } from '../ontario-summary-list';
+
+describe('ontario-summary-list', () => {
+	it('renders', async () => {
+		const page = await newSpecPage({
+			components: [OntarioSummaryList],
+			html: `<ontario-summary-list></ontario-summary-list>`,
+		});
+		expect(page.root).toEqualHtml(`
+      <ontario-summary-list>
+        <mock:shadow-root>
+          <slot></slot>
+        </mock:shadow-root>
+      </ontario-summary-list>
+    `);
+	});
+});
