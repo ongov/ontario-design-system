23e41e8b18014e4bb8f594ad07720608d224b996
Munta Islam <munta.islam@ontario.ca>
2026-05-26 16:08:00 -0400

    test(ontario-summary-list): add initial playwright e2e tests for default state

--- packages/ontario-design-system-component-library/src/components/ontario-summary-list/test/ontario-summary-list.e2e.ts
+++ packages/ontario-design-system-component-library/src/components/ontario-summary-list/test/ontario-summary-list.e2e.ts
@@ -1,11 +1,30 @@
-// import { newE2EPage } from '@stencil/core/testing';
+import { expect, Locator } from '@playwright/test';
+import { test } from '@stencil/playwright';
 
-// describe('ontario-summary-list', () => {
-// 	it('renders', async () => {
-// 		const page = await newE2EPage();
-// 		await page.setContent('<ontario-summary-list></ontario-summary-list>');
+test.describe('ontario-summary-list', () => {
+	let host: Locator;
 
-// 		const element = await page.find('ontario-summary-list');
-// 		expect(element).toHaveClass('hydrated');
-// 	});
-// });
+	test.beforeEach(async ({ page }) => {
+		await page.setContent(`
+			<ontario-summary-list caption="Personal information"></ontario-summary-list>
+		`);
+		await page.waitForChanges();
+		host = page.locator('ontario-summary-list').first();
+	});
+
+	test('renders and is hydrated', async () => {
+		await expect(host).toBeAttached();
+		await expect(host).toHaveClass('hydrated');
+	});
+
+	test('renders the caption as an h3 heading by default', async () => {
+		const heading = host.locator('h3.ontario-summary-list__heading');
+		await expect(heading).toBeAttached();
+		await expect(heading).toHaveText('Personal information');
+	});
+
+	test('renders the dl container', async () => {
+		const container = host.locator('dl.ontario-summary-list__container');
+		await expect(container).toBeAttached();
+	});
+});
