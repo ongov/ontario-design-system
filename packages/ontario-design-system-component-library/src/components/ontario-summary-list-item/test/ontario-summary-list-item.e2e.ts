a2a6fc54cb977bbf496af5993f96926aa11ab459
Munta Islam <islammuntasirul@gmail.com>
2026-05-29 10:05:00 -0400

    chore(ontario-summary-list-item): comment out e2e scaffold stub

--- packages/ontario-design-system-component-library/src/components/ontario-summary-list-item/test/ontario-summary-list-item.e2e.ts
+++ packages/ontario-design-system-component-library/src/components/ontario-summary-list-item/test/ontario-summary-list-item.e2e.ts
@@ -1,11 +1,11 @@
-import { newE2EPage } from '@stencil/core/testing';
+// import { newE2EPage } from '@stencil/core/testing';
 
-describe('ontario-summary-list-item', () => {
-	it('renders', async () => {
-		const page = await newE2EPage();
-		await page.setContent('<ontario-summary-list-item></ontario-summary-list-item>');
+// describe('ontario-summary-list-item', () => {
+// 	it('renders', async () => {
+// 		const page = await newE2EPage();
+// 		await page.setContent('<ontario-summary-list-item></ontario-summary-list-item>');
 
-		const element = await page.find('ontario-summary-list-item');
-		expect(element).toHaveClass('hydrated');
-	});
-});
+// 		const element = await page.find('ontario-summary-list-item');
+// 		expect(element).toHaveClass('hydrated');
+// 	});
+// });
