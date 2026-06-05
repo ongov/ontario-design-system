5fc25976b8875937594c522cc71f59b72212719e
Munta Islam <islammuntasirul@gmail.com>
2026-06-05 12:46:15 -0400

    fix(ontario-table): use getImageAssetSrcPath helper for highlight indicator
    JIRA: 2236

--- packages/ontario-design-system-component-library/src/components/ontario-table/ontario-table.tsx
+++ packages/ontario-design-system-component-library/src/components/ontario-table/ontario-table.tsx
@@ -7,6 +7,7 @@ import { validateTableColumns, validateTableRowOptions } from './utils/ontario-t
 import { ConsoleMessageClass } from '../../utils/console-message/console-message';
 import { ConsoleType } from '../../utils/console-message/console-message.enum';
 import { extractValuesByKey, organizeObjectKeys, removeObjectsBySpecificKey } from '../../utils/helper/utils';
+import { getImageAssetSrcPath } from '../../utils/helper/assets';
 
 /**
  * Ontario Table presents structured tabular data with accessible semantics.
@@ -240,15 +241,12 @@ export class OntarioTable implements Table {
 					return index === 0 ? (
 						<th scope="row" innerHTML={rowData.data[`${columns[0]}`]}>
 							{dataType === 'tableData' && rowData.highlight && (
-								<svg
+								<img
 									class="ontario-table--highlight-indicator"
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 11.3 22.6"
+									src={getImageAssetSrcPath('highlight-indicator.svg')}
+									alt=""
 									aria-hidden="true"
-									focusable="false"
-								>
-									<polygon points="0,0 11.3,11.3 0,22.6" />
-								</svg>
+								/>
 							)}
 						</th>
 					) : (
