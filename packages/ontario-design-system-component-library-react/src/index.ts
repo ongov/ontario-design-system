06a2ead055c8536066e5c727be05c3c592a8f8e8
Erin Carrieres <erin.carrieres@ontario.ca>
2026-06-16 11:46:45 -0400

    chore: export the taskStatuses enum

--- packages/ontario-design-system-component-library-react/src/index.ts
+++ packages/ontario-design-system-component-library-react/src/index.ts
@@ -1,2 +1,3 @@
 export * from './components/components.js';
 export { setAssetPath } from '@ongov/ontario-design-system-component-library/components/index.js';
+export { TaskStatuses } from '@ongov/ontario-design-system-component-library';
--- packages/ontario-design-system-component-library/src/index.ts
+++ packages/ontario-design-system-component-library/src/index.ts
@@ -1,2 +1,3 @@
 // export { Components, JSX } from './components';
 export * from './components';
+export { TaskStatuses } from './utils/common/task-statuses.enum';
