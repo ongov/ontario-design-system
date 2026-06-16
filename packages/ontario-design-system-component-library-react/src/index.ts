16fa1d129b16a8c8e72edddb5c919e2b24ed8d74
Erin Carrieres <erin.carrieres@ontario.ca>
2026-06-16 15:54:38 -0400

    chore: fixing build error

--- packages/ontario-design-system-component-library-react/src/index.ts
+++ packages/ontario-design-system-component-library-react/src/index.ts
@@ -1,3 +1,3 @@
 export * from './components/components.js';
 export { setAssetPath } from '@ongov/ontario-design-system-component-library/components/index.js';
-export { TaskStatuses } from '@ongov/ontario-design-system-component-library';
+export { TaskStatuses } from './task-statuses';
---
+++ packages/ontario-design-system-component-library-react/src/task-statuses.ts
@@ -0,0 +1,8 @@
+export const TaskStatuses = {
+	NotStarted: 'notStarted',
+	InProgress: 'inProgress',
+	Completed: 'completed',
+	CannotStartYet: 'cannotStartYet',
+	Error: 'error',
+	Optional: 'optional',
+} as const;
